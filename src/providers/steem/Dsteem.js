52c84386d6a813de498f8a350f381dcacaac6f9d
Hüseyin Terkir <hsynterkr@gmail.com>
2018-07-31 17:43:00 +0300

    Dsteem data provider improvements

--- src/providers/steem/Dsteem.js
+++ src/providers/steem/Dsteem.js
@@ -1,11 +1,7 @@
 import { Client } from 'dsteem';
 const client = new Client('https://api.steemit.com');
 
-import moment from 'moment';
-import { postSummary } from '../../utils/PostSummary';
-import { markDown2Html } from '../../utils/PostParser';
-
-const noImage = require('../../assets/noimage.png');
+import { parsePosts } from '../../utils/PostParser';
 
 import Remarkable from 'remarkable';
 var md = new Remarkable({
@@ -44,45 +40,6 @@ var md = new Remarkable({
     let posts = await client.database.getDiscussions(by, query);
     posts = await parsePosts(posts);
     return posts;
-  }
-
-  /**
-   * TODO move it to Utils
-   */
-  export const parsePosts = (posts) => {
-    posts.map(post => {
-      post.json_metadata = JSON.parse(post.json_metadata);
-      post.image = (post.json_metadata.image) ? post.json_metadata.image[0] : noImage.url;
-      post.pending_payout_value = parseFloat(post.pending_payout_value).toFixed(2);
-      post.created = moment.utc(post.created).local().fromNow();
-      post.vote_count = post.active_votes.length;
-      post.author_reputation = reputation(post.author_reputation);
-      post.avatar = `https://steemitimages.com/u/${post.author}/avatar/small`;
-      post.body = markDown2Html(post.body)
-      post.summary = postSummary(post.body, 200);
-      post.raw_body = post.body;
-      post.active_votes.sort((a,b) => {
-        return b.rshares - a.rshares
-      });
-      if (post.active_votes.length > 2) {
-        post.top_likers = [post.active_votes[0].voter, post.active_votes[1].voter, post.active_votes[2].voter]
-      }
-    });
-    return posts;
-  } 
-
-  /**
-   * TODO move it to Utils
-   */
-  export const reputation = (reputation) => {
-    if (reputation == null) return reputation;
-    reputation = parseInt(reputation);
-    let log = Math.log10(reputation);
-    log = log - 9;
-    log = log * 9;
-    log = log + 25;
-    log = Math.floor(log);
-    return log;
   }
 
   /**
