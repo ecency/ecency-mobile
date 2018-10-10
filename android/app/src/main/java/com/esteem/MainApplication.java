1f013c8bdc98ca2817f8e9d2e496dcc90f1a1227
Feruz <fmiboy@gmail.com>
2018-10-10 15:36:15 +0300

    small change

--- android/app/src/main/java/com/esteem/MainApplication.java
+++ android/app/src/main/java/com/esteem/MainApplication.java
@@ -3,6 +3,9 @@ package com.esteem;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.avishayil.rnrestart.ReactNativeRestartPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +28,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeRestartPackage(),
+            new LinearGradientPackage(),
+            new FastImageViewPackage(),
             new VectorIconsPackage(),
             new RealmReactPackage()
       );
