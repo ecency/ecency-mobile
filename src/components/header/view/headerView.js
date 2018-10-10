a6d12a7e6b00985d352a26baf5fdc1ce67568bc9
ue <ugurerdal93@gmail.com>
2018-10-10 23:23:18 +0300

    added popodfile

--- src/components/header/view/headerView.js
+++ src/components/header/view/headerView.js
@@ -35,9 +35,16 @@ class HeaderView extends Component {
     return (
       <SafeAreaView style={styles.container}>
         <StatusBar hidden={hideStatusBar} translucent />
-        <View style={styles.avatarWrapper}>
-          <FastImage style={styles.avatar} source={avatar || DEFAULT_IMAGE} />
-        </View>
+        <LinearGradient
+          start={{ x: 0.0, y: 0.25 }}
+          end={{ x: 0.5, y: 1.0 }}
+          locations={[0, 0.5, 0.6]}
+          colors={['#4c669f', '#3b5998', '#192f6a']}
+        >
+          <View style={styles.avatarWrapper}>
+            <FastImage style={styles.avatar} source={avatar} defaultSource={DEFAULT_IMAGE} />
+          </View>
+        </LinearGradient>
         <View style={styles.titleWrapper}>
           <Text style={styles.title}> eSteem Project </Text>
           <Text style={styles.subTitle}> @u-e (63)</Text>
