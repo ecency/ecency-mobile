11994929469f11b32052e169f663dc19fd30993b
ue <ugurerdal93@gmail.com>
2018-10-26 16:23:35 +0300

    created structure for upvote

---
+++ src/components/upvote/index.js
@@ -0,0 +1,5 @@
+import ExampleView from './view/exampleView';
+import ExampleContainer from './container/exampleContainer';
+
+export { ExampleView, ExampleContainer };
+export default ExampleContainer;
