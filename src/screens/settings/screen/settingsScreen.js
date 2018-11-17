30a61a02f885cdf0053571de56549bf8daf16459
ue <ugurerdal93@gmail.com>
2018-11-17 23:40:19 +0100

    created settings structure

---
+++ src/screens/settings/screen/settingsScreen.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+// Constants
+
+// Components
+
+class SettingsScreen extends Component {
+  /* Props
+    * ------------------------------------------------
+    *   @prop { type }    name                - Description....
+    */
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  // Component Life Cycles
+
+  // Component Functions
+
+  render() {
+    // eslint-disable-next-line
+    //const {} = this.props;
+
+    // eslint-disable-next-line
+    return <View />;
+  }
+}
+
+export default SettingsScreen;
