7faa3e2823572d7207b1ce76ad2d4ca0ff988933
Feruz <fmiboy@gmail.com>
2018-12-05 19:46:36 +0200

    appcenter pre build

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Creates an .env from ENV variables for use with react-native-config
+ENV_WHITELIST=${ENV_WHITELIST:-"."}
+printf "Creating an .env file with the following whitelist:\n"
+printf "%s\n\n" $ENV_WHITELIST
+set | egrep -e $ENV_WHITELIST | egrep -v "^_" | egrep -v "WHITELIST" > .env
+printf "\n.env created with contents:\n"
+cat .env
