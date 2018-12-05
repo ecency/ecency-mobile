e78bfbc6042cd251420cf322fd190f8aa5bad1f2
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2018-12-05 14:43:07 +0300

    Fixed release crash on Android

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+import 'core-js';
 import { AppRegistry } from 'react-native';
-import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+global.Intl = require('intl');
+
+AppRegistry.registerComponent(appName, () => require('./App').default);
