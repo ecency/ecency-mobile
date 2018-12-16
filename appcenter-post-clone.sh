24a8a7e1b809b050ca8a7c9707603ee896ade7af
Feruz <fmiboy@gmail.com>
2018-12-16 20:31:20 +0200

    upd post-clone

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,10 +1,6 @@
-#!/bin/sh
-
-# please specify required Node.js version
-NODE_VERSION=8.12.0
-
-# workaround to override the v8 alias
-npm config delete prefix
-. ~/.bashrc
-nvm install "$NODE_VERSION"
-nvm alias node8 "$NODE_VERSION"
+#!/usr/bin/env bash
+set -ex
+brew uninstall node@6
+NODE_VERSION="8.12.0"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
