45d825f4ce7eb4c6102f0d5e8fd622bff1c50388
Feruz <fmiboy@gmail.com>
2018-12-16 20:49:35 +0200

    u

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -4,3 +4,12 @@ printf "post-clone.sh\n"
 
 brew uninstall node@6
 brew install node@8
+
+# please specify required Node.js version
+NODE_VERSION=8.12.0
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node8 "$NODE_VERSION"
