a59acdd672dd6b9a9f24e0a20b949f4995ff49fe
Feruz <fmiboy@gmail.com>
2018-12-16 21:11:39 +0200

    up

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,8 +3,9 @@
 printf "post-clone.sh\n"
 
 brew uninstall node@6
-brew unlink node
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+nvm use --delete-prefix v6.14.4 --silent
 
 nvm install 8.12.0
