61872af31ef9927d2903b97f7e48cd6f8c0a697c
Feruz <fmiboy@gmail.com>
2018-12-16 20:38:51 +0200

    u

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-set -ex
+
+printf "post-clone.sh\n"
+
 brew uninstall node@6
-NODE_VERSION="8.12.0"
-curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
-sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+brew install node@8
