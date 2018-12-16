d11b8ec717beef82ec3323fba77fbeccd251996b
Feruz <fmiboy@gmail.com>
2018-12-16 20:21:24 +0200

    post-clone-script

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# please specify required Node.js version
+NODE_VERSION=8.12.0
+
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node8 "$NODE_VERSION"
