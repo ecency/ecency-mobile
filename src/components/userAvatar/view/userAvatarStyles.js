8abc5872ed59116679345e7e1d7c332c29359f37
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2018-12-17 23:12:12 +0300

    Fixed user avatar background color

--- src/components/userAvatar/view/userAvatarStyles.js
+++ src/components/userAvatar/view/userAvatarStyles.js
@@ -2,7 +2,6 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
   avatar: {
-    borderWidth: 0.75,
-    borderColor: '$borderColor',
+    backgroundColor: '$primaryBackgroundColor',
   },
 });
