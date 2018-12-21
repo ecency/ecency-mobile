c1376d111d3c3bacf208e175e74d97bb52ee327d
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2018-12-21 11:59:46 +0300

    Moved some codes about data moved to login container

--- src/screens/login/container/loginContainer.js
+++ src/screens/login/container/loginContainer.js
@@ -1,11 +1,21 @@
 import React, { PureComponent } from 'react';
+import { Alert, Linking } from 'react-native';
 import { connect } from 'react-redux';
 
 // Services and Actions
+import { Login } from '../../../providers/steem/auth';
+import { lookupAccounts } from '../../../providers/steem/dsteem';
+import {
+  failedAccount,
+  addOtherAccount,
+  updateCurrentAccount,
+} from '../../../redux/actions/accountAction';
+import { login as loginAction, openPinCodeModal } from '../../../redux/actions/applicationActions';
 
 // Middleware
 
 // Constants
+import ROUTES from '../../../constants/routeNames';
 
 // Utilities
 
@@ -21,15 +31,58 @@ import LoginScreen from '../screen/loginScreen';
 class LoginContainer extends PureComponent {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      isLoading: false,
+    };
   }
 
   // Component Life Cycle Functions
 
   // Component Functions
 
+  _handleOnPressLogin = (username, password) => {
+    const { dispatch, setPinCodeState } = this.props;
+
+    this.setState({ isLoading: true });
+
+    Login(username, password)
+      .then((result) => {
+        if (result) {
+          dispatch(updateCurrentAccount({ ...result }));
+          dispatch(addOtherAccount({ username: result.name }));
+          dispatch(openPinCodeModal());
+          setPinCodeState({ navigateTo: ROUTES.DRAWER.MAIN });
+          dispatch(loginAction(true));
+        }
+      })
+      .catch((err) => {
+        // TODO: Change with global error handling
+        Alert.alert('Error', err.message);
+        dispatch(failedAccount(err.message));
+        this.setState({ isLoading: false });
+      });
+  };
+
+  _getAccountsWithUsername = async (username) => {
+    const validUsers = await lookupAccounts(username);
+    return validUsers;
+  };
+
+  _handleSignUp = () => {
+    Linking.openURL('https://signup.steemit.com/?ref=esteem').catch(err => alert('An error occurred', err));
+  };
+
   render() {
-    return <LoginScreen {...this.props} />;
+    const { isLoading } = this.state;
+    return (
+      <LoginScreen
+        handleOnPressLogin={this._handleOnPressLogin}
+        getAccountsWithUsername={this._getAccountsWithUsername}
+        handleSignUp={this._handleSignUp}
+        isLoading={isLoading}
+      />
+    );
   }
 }
 
