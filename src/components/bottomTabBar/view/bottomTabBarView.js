4d891cd4449297779683dc06ed3c91e57aa33ca1
u-e <ugurerdal93@gmail.com>
2018-12-24 12:58:54 +0300

    routing usagusage from constant && enhanced

--- src/components/bottomTabBar/view/bottomTabBarView.js
+++ src/components/bottomTabBar/view/bottomTabBarView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { TouchableOpacity, SafeAreaView } from 'react-native';
 import { connect } from 'react-redux';
 import ViewOverflow from 'react-native-view-overflow';
@@ -13,7 +13,7 @@ import { updateActiveBottomTab } from '../../../redux/actions/uiAction';
 // Styles
 import styles from './bottomTabBarStyles';
 
-class BottomTabBarView extends Component {
+class BottomTabBarView extends PureComponent {
   /* Props
    * ------------------------------------------------
    *   @prop { type }    name                - Description....
