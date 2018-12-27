012356ced597d627a6b2d83dd3b736dfbfa6f397
Feruz <fmiboy@gmail.com>
2018-12-27 12:55:51 +0200

    pre-build

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,7 +3,7 @@
 printf "Old .env file:\n"
 cat .env
 printf "Started script:\n"
-ENV_WHITELIST=${ENV_WHITELIST:-"/ACTIVITY|WEBSOCKET|BACKEND|API|TOKEN|URL/"}
+ENV_WHITELIST=${ENV_WHITELIST:-"/ACTIVITY|WEBSOCKET|BACKEND|API|TOKEN|PIN|URL/"}
 printf "Creating an .env file with the following whitelist:\n"
 printf "%s\n\n" $ENV_WHITELIST
 set | egrep -e $ENV_WHITELIST | egrep -v "^_" | egrep -v "WHITELIST" | egrep -v "USER-DEFINED" > .env
