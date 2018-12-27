83790a2f3aa79eec6d753b82d0ace0c59276e1c1
Feruz <fmiboy@gmail.com>
2018-12-27 10:46:57 +0200

    post-clone

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,6 +2,8 @@
 
 printf "post-clone.sh\n"
 
+node -v
+
 # please specify required Node.js version
 NODE_VERSION=8.12.0
 
@@ -10,3 +12,7 @@ npm config delete prefix
 . ~/.bashrc
 nvm install "$NODE_VERSION"
 nvm alias node8 "$NODE_VERSION"
+
+node -v
+
+printf "end of post-clone.sh\n"
