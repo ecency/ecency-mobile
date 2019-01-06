15e3f4702d724e734a99cad137b972572216d644
Fil Dunsky <filipp.dunsky@gmail.com>
2019-01-06 13:39:40 +0300

    Russian translation added

--- src/config/locales/index.js
+++ src/config/locales/index.js
@@ -1,6 +1,7 @@
 export default {
   'en-US': require('./en-US.json'),
+  'ru-RU': require('./ru-RU.json'),
   'tr-TR': require('./tr-TR.json'),
 };
 
-export const locales = [{ id: 'en-US', name: 'English' }, { id: 'tr-TR', name: 'Türkçe' }];
+export const locales = [{ id: 'en-US', name: 'English' }, { id: 'ru-RU', name: 'Русский' }, { id: 'tr-TR', name: 'Türkçe' }];
