9d1cce4781cd497e20dd1de7fff39797efe90332
u-e <ugurerdal93@gmail.com>
2019-01-15 13:43:50 +0300

    updated date added '~'

--- src/utils/post.js
+++ src/utils/post.js
@@ -1,1 +1,14 @@
-// ^ (http: \/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$
+import parseToken from './parseToken';
+import { isEmptyContentDate } from './time';
+
+export const postSumTotal = (content) => {
+  if (content.pending_payout_value && isEmptyContentDate(content.last_payout)) {
+    return content.total_payout_value
+      ? parseToken(content.total_payout_value) + parseToken(content.pending_payout_value)
+      : 0;
+  }
+
+  return content.total_payout_value
+    ? parseToken(content.total_payout_value) + parseToken(content.curator_payout_value)
+    : 0;
+};
