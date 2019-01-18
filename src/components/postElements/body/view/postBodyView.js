2a84d7994eb2886316adbafbc8e0194be411d55a
u-e <ugurerdal93@gmail.com>
2019-01-18 12:44:44 +0300

    closed #275

--- src/components/postElements/body/view/postBodyView.js
+++ src/components/postElements/body/view/postBodyView.js
@@ -1,7 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
-import {
-  Dimensions, Linking, Alert,
-} from 'react-native';
+import { Dimensions, Linking, Alert } from 'react-native';
 import { withNavigation } from 'react-navigation';
 import { injectIntl } from 'react-intl';
 
@@ -16,7 +14,7 @@ import { default as ROUTES } from '../../../../constants/routeNames';
 const WIDTH = Dimensions.get('window').width;
 const CUSTOM_RENDERERS = {
   // example
-  //center: () => <Text style={{ backgroundColor: 'blue', textAlign: 'center'}}>ugur</Text>,
+  // center: () => <Text style={{ backgroundColor: 'blue', textAlign: 'center'}}>ugur</Text>,
 };
 const DEFAULT_PROPS = {
   renderers: CUSTOM_RENDERERS,
@@ -100,6 +98,26 @@ class PostBody extends PureComponent {
 
     if (node.name === 'img') {
       node.attribs.style = 'text-align: center;';
+    }
+
+    if (node.name === 'div' && node.attribs && node.attribs.class) {
+      const _className = node.attribs.class;
+
+      if (_className === 'pull-right') {
+        node.attribs.style = 'text-align: right;';
+      }
+
+      if (_className === 'pull-left') {
+        node.attribs.style = 'text-align: left;';
+      }
+
+      if (_className === 'text-justify') {
+        node.attribs.style = 'text-align: justify; text-justify: inter-word; letter-spacing: 1.2px;';
+      }
+
+      if (_className === 'phishy') {
+        node.attribs.style = 'color: red';
+      }
     }
   };
 
