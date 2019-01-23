e819bd21801dab198ddb37c1dde308dd69976c2e
u-e <ugurerdal93@gmail.com>
2019-01-23 11:07:19 +0300

    updated card view

--- src/components/postCard/view/postCardView.js
+++ src/components/postCard/view/postCardView.js
@@ -87,11 +87,7 @@ class PostCardView extends Component {
             onPress={() => this._handleOnContentPress()}
           >
             {!isHideImage && (
-              <FastImage
-                source={_image}
-                style={styles.thumbnail}
-                defaultSource={DEFAULT_IMAGE}
-              />
+              <FastImage source={_image} style={styles.thumbnail} defaultSource={DEFAULT_IMAGE} />
             )}
             <View style={[styles.postDescripton]}>
               <Text style={styles.title}>{content.title}</Text>
