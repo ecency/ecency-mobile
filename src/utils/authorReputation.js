523e5a688541a31936a23685d4a7474d18c86dc8
u-e <ugurerdal93@gmail.com>
2019-02-18 22:25:35 +0300

    created comment filter

---
+++ src/utils/authorReputation.js
@@ -0,0 +1,24 @@
+export default (input) => {
+  if (input === 0) {
+    return 25;
+  }
+
+  if (!input) {
+    return input;
+  }
+
+  let neg = false;
+
+  if (input < 0) neg = true;
+
+  let reputationLevel = Math.log10(Math.abs(input));
+  reputationLevel = Math.max(reputationLevel - 9, 0);
+
+  if (reputationLevel < 0) reputationLevel = 0;
+
+  if (neg) reputationLevel *= -1;
+
+  reputationLevel = reputationLevel * 9 + 25;
+
+  return Math.floor(reputationLevel);
+};
