69a6f5fd5f8d9f0c457d838e0b979767d06b9d17
u-e <ugurerdal93@gmail.com>
2019-03-01 00:10:24 +0300

    added endpoint urls added arch created get put post and implemented

---
+++ src/providers/esteem/ePoint.js
@@ -0,0 +1,65 @@
+import ePointApi from '../../config/ePoint';
+
+export const userActivity = (us, ty, bl = '', tx = '') => {
+  const params = { us, ty };
+
+  if (bl) {
+    params.bl = bl;
+  }
+
+  if (tx) {
+    params.tx = tx;
+  }
+
+  return ePointApi.post('/usr-activity', params).then(res => res.data);
+};
+
+export const transfer = (sender, receiver, amount) => new Promise((resolve, reject) => {
+  ePointApi
+    .post('/transfer', {
+      se: sender,
+      re: receiver,
+      am: amount,
+    })
+    .then((res) => {
+      resolve(res.data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
+export const getUser = username => new Promise((resolve, reject) => {
+  ePointApi
+    .get(`/users/:${username}`)
+    .then((res) => {
+      resolve(res.data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
+export const getUserPoints = username => new Promise((resolve, reject) => {
+  ePointApi
+    .get(`/users/:${username}/points`)
+    .then((res) => {
+      resolve(res.data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
+
+export const claim = username => new Promise((resolve, reject) => {
+  ePointApi
+    .put('/claim', {
+      us: `${username}`,
+    })
+    .then((res) => {
+      resolve(res.data);
+    })
+    .catch((error) => {
+      reject(error);
+    });
+});
