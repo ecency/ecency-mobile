a4f00506a4dd83bb4d9e83a9abacf812ec627e38
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-04-15 14:10:03 +0300

    Completed transfer design

--- src/screens/transfer/screen/transferScreen.js
+++ src/screens/transfer/screen/transferScreen.js
@@ -1,12 +1,170 @@
-import React from 'react';
-import { Text } from 'react-native';
-import styles from './transferStyles';
+/* eslint-disable no-restricted-globals */
+import React, { Fragment, Component } from 'react';
+import { Text, View } from 'react-native';
+import ActionSheet from 'react-native-actionsheet';
+import { injectIntl } from 'react-intl';
 
+import { BasicHeader } from '../../../components/basicHeader';
+import { TextInput } from '../../../components/textInput';
+import { TransferFormItem } from '../../../components/transferFormItem';
+import { MainButton } from '../../../components/mainButton';
+import { DropdownButton } from '../../../components/dropdownButton';
+import { UserAvatar } from '../../../components/userAvatar';
+import { Icon } from '../../../components/icon';
+
+import styles from './transferStyles';
 /* Props
  * ------------------------------------------------
  *   @prop { type }    name                - Description....
  */
 
-const TransferView = ({ x, y, z }) => <Text>test</Text>;
+class TransferView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      from: props.accounts[0].username,
+      destination: '',
+      amount: '',
+      memo: '',
+      isUsernameValid: false,
+    };
+  }
 
-export default TransferView;
+  // Component Life Cycles
+
+  // Component Functions
+  _setState = (key, value) => {
+    const { getAccountsWithUsername } = this.props;
+
+    if (key) {
+      switch (key) {
+        case 'destination':
+          getAccountsWithUsername(value).then((res) => {
+            const isValid = res.includes(value);
+
+            this.setState({ isUsernameValid: isValid });
+          });
+          this.setState({ [key]: value });
+          break;
+        case 'amount':
+          if (!isNaN(value)) this.setState({ [key]: value });
+          break;
+
+        default:
+          this.setState({ [key]: value });
+          break;
+      }
+    }
+  };
+
+  _handleTransferAction = () => {
+    const { transferToAccount } = this.props;
+    const {
+      from, destination, amount, memo,
+    } = this.state;
+
+    transferToAccount(from, destination, amount, memo);
+  };
+
+  _renderInput = (placeholder, state) => (
+    <TextInput
+      style={styles.input}
+      onChangeText={text => this._setState(state, text)}
+      value={this.state[state]}
+      placeholder={placeholder}
+      placeholderTextColor="#c1c5c7"
+      autoCapitalize="none"
+      keyboardType="numeric"
+    />
+  );
+
+  _renderDropdown = accounts => (
+    <DropdownButton
+      style={styles.dropdown}
+      options={accounts.map(item => item.username)}
+      defaultText={accounts[0].username}
+      selectedOptionIndex={0}
+      onSelect={(index, value) => this.setState({ from: value })}
+    />
+  );
+
+  _renderDescription = text => <Text style={styles.description}>{text}</Text>;
+
+  render() {
+    const { accounts, intl } = this.props;
+    const { destination, isUsernameValid, amount } = this.state;
+
+    return (
+      <Fragment>
+        <BasicHeader title={intl.formatMessage({ id: 'transfer.title' })} />
+        <View style={styles.container}>
+          <View style={styles.topContent}>
+            <UserAvatar
+              username={accounts[0].username}
+              size="xl"
+              style={styles.userAvatar}
+              noAction
+            />
+            <Icon style={styles.icon} name="arrow-forward" iconType="MaterialIcons" />
+            <UserAvatar username={destination} size="xl" style={styles.userAvatar} noAction />
+          </View>
+          <View style={styles.middleContent}>
+            <TransferFormItem
+              label={intl.formatMessage({ id: 'transfer.from' })}
+              rightComponent={() => this._renderDropdown(accounts)}
+            />
+            <TransferFormItem
+              label={intl.formatMessage({ id: 'transfer.to' })}
+              rightComponent={() => this._renderInput(
+                intl.formatMessage({ id: 'transfer.to_placeholder' }),
+                'destination',
+              )
+              }
+            />
+            <TransferFormItem
+              label={intl.formatMessage({ id: 'transfer.amount' })}
+              rightComponent={() => this._renderInput('Amount', 'amount')}
+            />
+            <TransferFormItem
+              rightComponent={() => this._renderDescription(intl.formatMessage({ id: 'transfer.amount_desc' }))
+              }
+            />
+            <TransferFormItem
+              label={intl.formatMessage({ id: 'transfer.memo' })}
+              rightComponent={() => this._renderInput(intl.formatMessage({ id: 'transfer.memo_placeholder' }), 'memo')
+              }
+            />
+            <TransferFormItem
+              rightComponent={() => this._renderDescription(intl.formatMessage({ id: 'transfer.memo_desc' }))
+              }
+            />
+          </View>
+          <View style={styles.bottomContent}>
+            <MainButton
+              style={styles.button}
+              isDisable={!(amount && isUsernameValid)}
+              onPress={() => this.ActionSheet.show()}
+            >
+              <Text style={styles.buttonText}>NEXT</Text>
+            </MainButton>
+          </View>
+        </View>
+        <ActionSheet
+          ref={o => (this.ActionSheet = o)}
+          options={[
+            intl.formatMessage({ id: 'alert.confirm' }),
+            intl.formatMessage({ id: 'alert.cancel' }),
+          ]}
+          title={intl.formatMessage({ id: 'transfer.information' })}
+          cancelButtonIndex={1}
+          destructiveButtonIndex={0}
+          onPress={(index) => {
+            index === 0 ? this._handleTransferAction() : null;
+          }}
+        />
+      </Fragment>
+    );
+  }
+}
+
+export default injectIntl(TransferView);
