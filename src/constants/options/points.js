30ce2b8d43c40fde240ce17591114247531a467d
u-e <ugurerdal93@gmail.com>
2019-04-27 16:47:54 +0300

    changed icons layout

--- src/constants/options/points.js
+++ src/constants/options/points.js
@@ -1,38 +1,75 @@
 export default {
   100: {
-    icon: 'post',
-    textKey: 'points.post_title',
-    iconType: 'MaterialIcons',
+    icon: 'pencil',
+    textKey: 'points.post_title',
+    nameKey: 'points.post',
+    iconType: 'MaterialCommunityIcons',
+    point: 15,
   },
   110: {
     icon: 'comment-text-outline',
     textKey: 'points.comment_title',
+    nameKey: 'points.comment',
     iconType: 'MaterialCommunityIcons',
+    point: 5,
   },
   120: {
     icon: 'upcircle',
     textKey: 'points.vote_title',
+    nameKey: 'points.vote',
     iconType: 'AntDesign',
+    point: 0.1,
   },
   130: {
     icon: 'repeat',
     textKey: 'points.reblog_title',
+    nameKey: 'points.reblog',
     iconType: 'MaterialIcons',
+    point: 1,
   },
   10: {
     icon: 'ticket',
     textKey: 'points.checkin_title',
+    nameKey: 'points.checkin',
     iconType: 'MaterialCommunityIcons',
+    point: 0.2,
   },
   20: {
     icon: 'person',
     textKey: 'points.login_title',
+    nameKey: 'points.login',
     iconType: 'MaterialIcons',
+    point: 100,
   },
   30: {
     icon: 'check-decagram',
     textKey: 'points.checkin_extra_title',
+    nameKey: 'points.checkin_extra',
     iconType: 'MaterialCommunityIcons',
-
+    point: 10,
   },
 };
+
+export const POINTS_KEYS = [
+  {
+    type: 100,
+  },
+  {
+    type: 110,
+  },
+  {
+    type: 120,
+  },
+  {
+    type: 130,
+  },
+  {
+    type: 10,
+  },
+  {
+    type: 20,
+  },
+  {
+    type: 30,
+  },
+];
