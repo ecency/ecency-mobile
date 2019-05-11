3d80465c4b5564e1b1c14488a26d262aec9bd8f3
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-05-11 15:06:11 +0300

    Changed app center log level

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import { AppRegistry } from 'react-native';
+import AppCenter from 'appcenter';
 import codePush from 'react-native-code-push';
 import { name as appName } from './app.json';
 import 'core-js';
 import 'intl';
 
+
 // set check frequency options
 const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_START };
 const eSteemApp = codePush(codePushOptions)(require('./App').default);
+
+AppCenter.setLogLevel(AppCenter.LogLevel.VERBOSE);
 
 AppRegistry.registerComponent(appName, () => eSteemApp);
