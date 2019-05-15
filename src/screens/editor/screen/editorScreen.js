8921aa52cf963017d4a514c9919d6500362b66e3
uğur erdal <ugurerdal93@gmail.com>
2019-05-15 19:45:09 +0300

    Update src/screens/editor/screen/editorScreen.js
    Co-Authored-By: Mustafa Buyukcelebi <mustafa-buyukcelebi@hotmail.com>

--- src/screens/editor/screen/editorScreen.js
+++ src/screens/editor/screen/editorScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { injectIntl } from 'react-intl';
-import { get } from 'lodash';
+import get from 'lodash/get';
 
 // Utils
 import { getWordsCount } from '../../../utils/editor';
