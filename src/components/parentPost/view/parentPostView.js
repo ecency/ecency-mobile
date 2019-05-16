255fc72c67bbeec572b74b5b0b01bc37e9f1cb04
u-e <ugurerdal93@gmail.com>
2019-05-16 23:42:24 +0300

    enhanced routes to post screen

--- src/components/parentPost/view/parentPostView.js
+++ src/components/parentPost/view/parentPostView.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import get from 'lodash/get';
 import { default as ROUTES } from '../../../constants/routeNames';
+
 
 import styles from './parentPostStyles';
 
-const ParentPost = ({ post, navigation }) => (
-  <View style={styles.container}>
-    <TouchableOpacity
-      onPress={() => (navigation && navigation.navigate
-        ? navigation.navigate({
-          routeName: ROUTES.SCREENS.POST,
-          params: {
-            content: post,
-          },
-          key: post.permlink,
-        })
-        : null)
+const ParentPost = (props) => {
+  const { navigation, post } = props;
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => (get(navigation, 'navigate')
+          ? navigation.navigate({
+            routeName: ROUTES.SCREENS.POST,
+            params: {
+              content: post,
+            },
+            key: post.permlink,
+          })
+          : null)
       }
-    >
-      <Text style={styles.title}>{post.title}</Text>
-      <Text style={styles.description}>{post.summary}</Text>
-    </TouchableOpacity>
-  </View>
-);
+      >
+        <Text style={styles.title}>{get(post, 'title')}</Text>
+        <Text style={styles.description}>{get(post, 'summary')}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 export default withNavigation(ParentPost);
