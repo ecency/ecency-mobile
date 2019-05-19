3aae7bb958a2e70b34a8da1389b3bdcb3e4212d8
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-05-19 13:31:08 +0300

    revert for android build

--- android/app/src/main/java/app/esteem/mobile/MainApplication.java
+++ android/app/src/main/java/app/esteem/mobile/MainApplication.java
@@ -21,6 +21,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.facebook.react.BuildConfig;
 
 import java.util.Arrays;
 import java.util.List;
