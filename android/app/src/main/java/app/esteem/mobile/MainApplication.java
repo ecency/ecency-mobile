58ca5cec86ae494542ce3e4d9635faacb4b73fb1
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-05-23 15:27:00 +0300

    Added bugsnag

--- android/app/src/main/java/app/esteem/mobile/MainApplication.java
+++ android/app/src/main/java/app/esteem/mobile/MainApplication.java
@@ -3,6 +3,7 @@ package app.esteem.mobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.bugsnag.BugsnagReactNative;
 import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.microsoft.codepush.react.CodePush;
 import io.realm.react.RealmReactPackage;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            BugsnagReactNative.getPackage(),
             new RNVersionNumberPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG),
             new RealmReactPackage(),
