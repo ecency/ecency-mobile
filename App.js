0bfe671f7093bb23b2816862cb228d8e808319ee
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-05-23 14:06:32 +0300

    Removed sentry

--- App.js
+++ App.js
@@ -1,18 +1,3 @@
-import { Sentry, SentryLog } from 'react-native-sentry';
-import codePush from 'react-native-code-push';
 import App from './src/index';
-
-Sentry.config('https://aeaa78debe6d428984f9823f4aee0681@sentry.io/1457345', {
-  deactivateStacktraceMerging: false,
-  logLevel: SentryLog.Verbose,
-  disableNativeIntegration: false,
-  handlePromiseRejection: true,
-}).install();
-codePush.getUpdateMetadata().then((update) => {
-  if (update) {
-    Sentry.setVersion(`${update.appVersion}-codepush:${update.label}`);
-  }
-});
-
 
 export default App;
