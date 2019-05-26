82cd0bab38a13385ce0a0c0ba93da1b95cd48c85
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-05-26 14:26:52 +0300

    Removed test codes

--- App.js
+++ App.js
@@ -2,6 +2,5 @@ import { Client } from 'bugsnag-react-native';
 import App from './src/index';
 
 const bugsnag = new Client('209f4246554ad209c903a45106669b9f');
-bugsnag.notify(new Error('test notify1112222'));
 
 export default App;
