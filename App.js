5b76e87c02329ce51e0f6ab762faf00fdad582f8
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-05-30 18:40:24 +0300

    Bugsnag implementation

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
+import { Client } from 'bugsnag-react-native';
+import Config from 'react-native-config';
 import App from './src/index';
+
+const bugsnag = new Client(Config.BUGSNAG_API_KEY);
+
+bugsnag.notify(new Error('test'));
 
 export default App;
