7d3de24b19f5f093348c15554bc0d61897eacad9
u-e <ugurerdal93@gmail.com>
2019-07-21 14:00:14 +0300

    created boost screen new design

--- src/screens/boost/screen/boostScreen.js
+++ src/screens/boost/screen/boostScreen.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import { injectIntl } from 'react-intl';
 import { View, Text } from 'react-native';
+import get from 'lodash/get';
 
 // Components
 import { BasicHeader } from '../../../components/basicHeader';
@@ -13,19 +14,20 @@ import styles from './boostScreenStyles';
 
 const BOOST_DATA = [
   {
+    id: 0,
     name: '10000 ESTM',
     priceText: '100$',
     price: 100,
     description: 'BEST DEAL!',
   },
-  { name: '5000 ESTM', priceText: '50.00$', price: 50, description: 'POPULAR' },
-  { name: '1000 ESTM', priceText: '10.00$', price: 10, description: '' },
-  { name: '500 ESTM', priceText: '5.00$', price: 5, description: '' },
-  { name: '200 ESTM', priceText: '2.00$', price: 2, description: '' },
-  { name: '100 ESTM', priceText: '1.00$', price: 1, description: '' },
+  { id: 1, name: '5000 ESTM', quantity: 500, price: 50, description: 'POPULAR' },
+  { id: 2, name: '1000 ESTM', quantity: 10000, price: 10, description: '' },
+  { id: 3, name: '500 ESTM', quantity: 500, price: 5, description: '' },
+  { id: 4, name: '200 ESTM', quantity: 200, price: 2, description: '' },
+  { id: 5, name: '100 ESTM', quantity: 100, price: 1, description: '' },
 ];
 
-class BoostScreen extends Component {
+class BoostScreen extends PureComponent {
   /* Props
    * ------------------------------------------------
    *   @prop { type }    name                - Description....
@@ -55,33 +57,37 @@ class BoostScreen extends Component {
         />
 
         {BOOST_DATA.map(item => (
-          <View style={styles.boostLine}>
-            {item.description && (
+          <View style={styles.boostLine} key={get(item, 'id')}>
+            {!!get(item, 'description', null) && (
               <View style={styles.descriptionWrapper}>
-                <Text>{item.description}</Text>
+                <Fragment>
+                  <Text style={styles.description}>{get(item, 'description')}</Text>
+                  <View style={styles.triangle} />
+                </Fragment>
               </View>
             )}
-
-            <MainButton
-              style={styles.button}
-              onPress={() => alert(selectedBoost)}
-              height={50}
-              text={intl.formatMessage({
-                id: 'boost.buy',
-              })}
-              isDisable={false}
-              isLoading={false}
-            >
-              <View style={styles.buttonContent}>
-                <Text style={styles.buttonText}>{item.name}</Text>
-                <View style={styles.buttonIconWrapper}>
-                  <Icon name="add" iconType="MaterialIcons" color="#357ce6" size={23} />
+            <View style={styles.buttonWrapper}>
+              <MainButton
+                style={styles.button}
+                onPress={() => this.setState({ selectedBoost: get(item, 'id') })}
+                height={50}
+                text={intl.formatMessage({
+                  id: 'boost.buy',
+                })}
+                isDisable={false}
+                isLoading={false}
+              >
+                <View style={styles.buttonContent}>
+                  <Text style={styles.buttonText}>{get(item, 'name')}</Text>
+                  <View style={styles.buttonIconWrapper}>
+                    <Icon name="add" iconType="MaterialIcons" color="#357ce6" size={23} />
+                  </View>
                 </View>
-              </View>
-            </MainButton>
+              </MainButton>
+            </View>
 
             <View style={styles.priceWrapper}>
-              <Text>{item.priceText}</Text>
+              <Text style={styles.priceText}>{`$${get(item, 'price', 0).toFixed(2)}`}</Text>
             </View>
           </View>
         ))}
