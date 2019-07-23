dcc80af4c71de9fd769a08360bea763477e1fbc1
u-e <ugurerdal93@gmail.com>
2019-07-23 19:53:05 +0300

    fixed styles added iap still wip

--- src/screens/boost/container/boostContainer.js
+++ src/screens/boost/container/boostContainer.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Platform } from 'react-native';
+import RNIap, {
+  purchaseErrorListener,
+  purchaseUpdatedListener,
+  ProductPurchase,
+  PurchaseError,
+} from 'react-native-iap';
 // import { Alert } from 'react-native';
 import { injectIntl } from 'react-intl';
 
@@ -28,11 +35,52 @@ class BoostContainer extends Component {
   }
 
   // Component Life Cycle Functions
+  async componentDidMount() {
+    try {
+      const itemSkus = Platform.select({
+        ios: ['app.esteem.mobile.ios.099_points'],
+        android: ['com.example.099points'],
+      });
+      // await RNIap.prepare();
+      const products = await RNIap.getProducts([
+        '099points',
+        '199points',
+        '499points',
+        '999points',
+        '4999points',
+        '9999points',
+      ]);
+      // this.setState({ items });
+      console.log(products);
+    } catch (err) {
+      console.warn(err);
+    }
+
+    this.purchaseUpdateSubscription = purchaseUpdatedListener((purchase: ProductPurchase) => {
+      console.log('purchaseUpdatedListener', purchase);
+      const receipt = purchase.transactionReceipt;
+      if (receipt) {
+        alert('done');
+      }
+    });
+
+    this.purchaseErrorSubscription = purchaseErrorListener((error: PurchaseError) => {
+      console.warn('purchaseErrorListener', error);
+    });
+  }
 
   // Component Functions
 
+  _purchase = async sku => {
+    try {
+      await RNIap.requestPurchase('099points', false);
+    } catch (err) {
+      console.warn(err.code, err.message);
+    }
+  };
+
   render() {
-    return <BoostScreen />;
+    return <BoostScreen purchase={this._purchase} />;
   }
 }
 
