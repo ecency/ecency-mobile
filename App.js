0c814810ab550557cc374238a268eb694154913c
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-08-05 14:42:23 +0300

    Added esteem backend after puchase action

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import { Client } from 'bugsnag-react-native';
-import Config from 'react-native-config';
+// eslint-disable-next-line
+import bugsnag from './src/config/bugsnag';
 import App from './src/index';
-
-const bugsnag = new Client(Config.BUGSNAG_API_KEY);
 
 export default App;
