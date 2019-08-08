d2f806b759aa917551ad4cdb0ecbae3fd4362e54
u-e <ugurerdal93@gmail.com>
2019-08-08 12:35:28 +0300

    removed ui from blacklist

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -13,7 +13,7 @@ const persistConfig = {
   // Storage Method (React Native)
   storage: AsyncStorage,
   // Blacklist (Don't Save Specific Reducers)
-  blacklist: ['nav', 'application', 'ui'],
+  blacklist: ['nav', 'application'],
 };
 
 // Middleware: Redux Persist Persisted Reducer
