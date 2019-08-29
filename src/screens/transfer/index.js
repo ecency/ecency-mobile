31ae6f594c99c28f2c345d556b39a61a4fbc199c
ue <ugurerdal93@gmail.com>
2019-08-29 23:48:13 +0300

    fixed transfer issue for steem and points

--- src/screens/transfer/index.js
+++ src/screens/transfer/index.js
@@ -42,6 +42,7 @@ const Transfer = ({ navigation }) => (
               handleOnModalClose={handleOnModalClose}
               accountType={accountType}
               currentAccountName={currentAccountName}
+              selectedAccount={selectedAccount}
             />
           );
         case 'delegate':
