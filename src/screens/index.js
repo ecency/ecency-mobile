3106ec63b22dab561e30210eb62d84e033666450
ue <ugurerdal93@gmail.com>
2019-09-20 00:19:31 +0300

    refactored part 1 with container

--- src/screens/index.js
+++ src/screens/index.js
@@ -10,15 +10,15 @@ import { Notification } from './notification';
 import { PinCode } from './pinCode';
 import { Points } from './points';
 import { Post } from './post';
-import { Profile } from './profile';
 import { SearchResult } from './searchResult';
 import { Settings } from './settings';
-import Voters from './voters';
+import Profile from './profile/screen/profileScreen';
+import ProfileEdit from './profileEdit/screen/profileEditScreen';
+import Reblogs from './reblogs';
+import Redeem from './redeem/screen/redeemScreen';
 import SteemConnect from './steem-connect/steemConnect';
 import Transfer from './transfer';
-import Reblogs from './reblogs';
-import ProfileEdit from './profileEdit/screen/profileEditScreen';
-import Redeem from './redeem/screen/redeemScreen';
+import Voters from './voters';
 
 export {
   Bookmarks,
@@ -36,10 +36,10 @@ export {
   Profile,
   ProfileEdit,
   Reblogs,
+  Redeem,
   SearchResult,
   Settings,
   SteemConnect,
   Transfer,
   Voters,
-  Redeem,
 };
