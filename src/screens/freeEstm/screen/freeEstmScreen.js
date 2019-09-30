4674781bca1ab666473d647bd98ab64669e6af29
ue <ugurerdal93@gmail.com>
2019-09-30 22:23:42 +0300

    added after spin screen

--- src/screens/freeEstm/screen/freeEstmScreen.js
+++ src/screens/freeEstm/screen/freeEstmScreen.js
@@ -7,7 +7,7 @@ import { PointsContainer } from '../../../containers';
 
 // Components
 import { BasicHeader } from '../../../components/basicHeader';
-import { BoostIndicatorAnimation, MainButton } from '../../../components';
+import { BoostIndicatorAnimation, MainButton, Icon } from '../../../components';
 
 import ESTM_TAGS from '../../../assets/estmTags.png';
 
@@ -25,6 +25,7 @@ class FreeEstmScreen extends PureComponent {
     this.state = {
       isSpinning: false,
       spinRight: 1,
+      earnedPoints: null,
     };
   }
 
@@ -35,11 +36,19 @@ class FreeEstmScreen extends PureComponent {
       isSpinning: true,
       spinRight: spinRight - 1,
     });
+
+    setTimeout(() => {
+      this.setState({ isSpinning: false, earnedPoints: 10 });
+    }, 8000);
+  };
+
+  _buySpinRight = () => {
+    alert('buy me');
   };
 
   render() {
     const { intl } = this.props;
-    const { isSpinning, spinRight } = this.state;
+    const { isSpinning, spinRight, earnedPoints } = this.state;
 
     return (
       <PointsContainer>
@@ -56,16 +65,39 @@ class FreeEstmScreen extends PureComponent {
                 )}
               </View>
               <View style={styles.spinnerWrapper}>
-                {!isSpinning && <Image source={ESTM_TAGS} style={styles.backgroundTags} />}
-                <BoostIndicatorAnimation isSpinning={isSpinning} />
+                {!isSpinning && spinRight > 0 && (
+                  <Image source={ESTM_TAGS} style={styles.backgroundTags} />
+                )}
+                <BoostIndicatorAnimation key={spinRight} isSpinning={isSpinning} />
+
+                {earnedPoints && (
+                  <View style={styles.descriptionWrapper}>
+                    <Fragment>
+                      <Text style={styles.description}>{`${earnedPoints} ESTM`}</Text>
+                      <View style={styles.triangle} />
+                    </Fragment>
+                  </View>
+                )}
 
                 <View style={{ flex: 1 }}>
                   {!isSpinning && (
                     <MainButton
-                      style={{ marginTop: 50 }}
-                      onPress={this._handleOnSpinPress}
-                      text="SPIN & WIN"
-                    />
+                      style={styles.button}
+                      onPress={spinRight > 0 ? this._handleOnSpinPress : this._buySpinRight}
+                    >
+                      <View style={styles.buttonContent}>
+                        <Text style={styles.buttonText}>
+                          {intl.formatMessage({
+                            id: spinRight > 0 ? 'free_estm.button' : 'free_estm.get_spin',
+                          })}
+                        </Text>
+                        {spinRight <= 0 && (
+                          <View style={styles.buttonIconWrapper}>
+                            <Icon name="add" iconType="MaterialIcons" color="#357ce6" size={23} />
+                          </View>
+                        )}
+                      </View>
+                    </MainButton>
                   )}
                 </View>
               </View>
