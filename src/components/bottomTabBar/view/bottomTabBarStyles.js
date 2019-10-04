160a93cc8d6cb0351054c8a4434e347e520aacdc
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-10-04 11:02:33 +0300

    Fixed auto navigate action for tabbar

--- src/components/bottomTabBar/view/bottomTabBarStyles.js
+++ src/components/bottomTabBar/view/bottomTabBarStyles.js
@@ -2,15 +2,10 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
   wrapper: {
-    flexDirection: 'row',
-    height: 56,
     width: '$deviceWidth',
-    backgroundColor: '$primaryBackgroundColor',
-    borderTopWidth: 0.1,
-    shadowOpacity: 0.1,
-    elevation: 10,
-  },
-  safeArea: {
-    backgroundColor: '$primaryBackgroundColor',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    zIndex: 99999,
   },
 });
