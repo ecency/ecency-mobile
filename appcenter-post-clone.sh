79a33c86f0f1a66fccab970f63089ef7b8804a40
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-10-11 23:29:28 +0300

    Fix for ios build

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -5,14 +5,14 @@ printf "post-clone.sh\n"
 node -v
 
 # please specify required Node.js version
-NODE_VERSION=8.12.0
+NODE_VERSION=8.16.0
 
 # workaround to override the v8 alias
 npm config delete prefix
 . ~/.bashrc
 nvm install "$NODE_VERSION"
 nvm alias node8 "$NODE_VERSION"
-nvm alias default v8.12.0
+nvm alias default v8.16.0
 node -v
 
 printf "end of post-clone.sh\n"
