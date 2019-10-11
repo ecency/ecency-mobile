81a04114d8a5fcfa333e56931169a7649b59d37e
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-10-11 10:42:28 +0300

    Fix for ios build

--- react-native.config.js
+++ react-native.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   dependencies: {
-    realm: {
-      platforms: {
-        ios: null, // disable iOS platform, other platforms will still autolink if provided
-      },
-    },
     'react-native-code-push': {
       platforms: {
         android: null, // disable Android platform, other platforms will still autolink if provided
