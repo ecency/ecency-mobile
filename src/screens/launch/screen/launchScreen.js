ee19930c4bfcf7e3351162d3fd21cc74418a8a92
ue <ugurerdal93@gmail.com>
2019-10-15 00:03:43 +0300

    added lottie

--- src/screens/launch/screen/launchScreen.js
+++ src/screens/launch/screen/launchScreen.js
@@ -1,22 +1,6 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import LottieView from 'lottie-react-native';
 
-import LOGO from '../../../assets/launch_screen.png';
-
-const LaunchScreen = () => (
-  <View
-    style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    <Image
-      source={LOGO}
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-      resizeMode="contain"
-    />
-  </View>
-);
+const LaunchScreen = () => <LottieView source={require('./animation.json')} autoPlay loop />;
 
 export default LaunchScreen;
