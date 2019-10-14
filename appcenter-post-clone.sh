1a929161971719aa1ae99275d1643e608ce71cf9
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-10-14 15:08:22 +0300

    Revert some changes

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,18 +1,18 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# printf "post-clone.sh\n"
+printf "post-clone.sh\n"
 
-# node -v
+node -v
 
-# # please specify required Node.js version
-# NODE_VERSION=8.16.0
+# please specify required Node.js version
+NODE_VERSION=8.16.0
 
-# # workaround to override the v8 alias
-# npm config delete prefix
-# . ~/.bashrc
-# nvm install "$NODE_VERSION"
-# nvm alias node8 "$NODE_VERSION"
-# nvm alias default v8.16.0
-# node -v
+# workaround to override the v8 alias
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm alias node8 "$NODE_VERSION"
+nvm alias default v8.16.0
+node -v
 
-# printf "end of post-clone.sh\n"
+printf "end of post-clone.sh\n"
