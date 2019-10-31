96c1e04a607c741a54c8a754456a0da887ce2ffb
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-10-31 16:44:00 +0300

    Added new configuration for report all api errors to bugsnag

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import Config from 'react-native-config';
+import bugsnag from './bugsnag';
 
 const api = axios.create({
   baseURL: Config.BACKEND_URL,
@@ -7,6 +8,11 @@ const api = axios.create({
     'Content-Type': 'application/json',
     'User-Agent': Config.USER_AGENT,
   },
+});
+
+api.interceptors.response.use(null, error => {
+  bugsnag.notify(error);
+  return Promise.reject(error);
 });
 
 export default api;
