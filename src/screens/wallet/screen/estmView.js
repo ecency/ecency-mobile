337316d7c4531b17309640b77b238ffa115d5ec3
ue <ugurerdal93@gmail.com>
2019-11-19 23:35:21 +0300

    seperated balance for currencys added first swipe item

---
+++ src/screens/wallet/screen/estmView.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import get from 'lodash/get';
+import { View } from 'react-native';
+
+import { Points } from '../../../components';
+import { PointsContainer } from '../../../containers';
+
+import globalStyles from '../../../globalStyles';
+
+const EstmView = ({ handleOnSelected, index, currentIndex }) => (
+  <View style={globalStyles.swipeItemWrapper}>
+    <PointsContainer>
+      {({
+        handleOnDropdownSelected,
+        claim,
+        fetchUserActivity,
+        isClaiming,
+        isLoading,
+        refreshing,
+        userActivities,
+        userPoints,
+        dropdownOptions,
+      }) => (
+        <Points
+          componentDidUpdate={() => handleOnSelected(userActivities, 'estm')}
+          index={index}
+          showIconList
+          claim={claim}
+          fetchUserActivity={fetchUserActivity}
+          isClaiming={isClaiming}
+          isLoading={isLoading}
+          refreshing={refreshing}
+          userActivities={userActivities}
+          unclaimedBalance={get(userPoints, 'unclaimed_points', 0)}
+          userBalance={get(userPoints, 'points')}
+          handleOnDropdownSelected={handleOnDropdownSelected}
+          type="estm"
+          dropdownOptions={dropdownOptions}
+          currentIndex={currentIndex}
+        />
+      )}
+    </PointsContainer>
+  </View>
+);
+
+export default EstmView;
---
+++ src/screens/wallet/screen/steemView.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import get from 'lodash/get';
+import { View } from 'react-native';
+
+import { Points } from '../../../components';
+import { SteemWalletContainer, AccountContainer } from '../../../containers';
+
+import globalStyles from '../../../globalStyles';
+
+const SteeemView = ({ handleOnSelected, index, currentIndex }) => (
+  <View style={globalStyles.swipeItemWrapper}>
+    <AccountContainer>
+      {({ currentAccount }) => (
+        <SteemWalletContainer selectedUser={currentAccount}>
+          {({
+            isClaiming,
+            claimRewardBalance,
+            handleOnWalletRefresh,
+            refreshing,
+            walletData,
+            steemPerMVests,
+            userActivities,
+            steemBalance,
+            isLoading,
+          }) => (
+            <Points
+              componentDidUpdate={() => handleOnSelected(userActivities, 'steem')}
+              index={index}
+              showIconList
+              claim={claimRewardBalance}
+              fetchUserActivity={handleOnWalletRefresh}
+              isClaiming={isClaiming}
+              isLoading={isLoading}
+              refreshing={refreshing}
+              userActivities={userActivities}
+              unclaimedBalance={0}
+              userBalance={steemBalance}
+              handleOnDropdownSelected={null}
+              type="steem"
+              dropdownOptions={[]}
+              currentIndex={currentIndex}
+            />
+          )}
+        </SteemWalletContainer>
+      )}
+    </AccountContainer>
+  </View>
+);
+
+export default SteeemView;
