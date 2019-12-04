99b958027b9f1a24f33aa7c0c66e48d8daf59cbf
feruz <fmiboy@gmail.com>
2019-12-04 14:54:14 +0200

    clean unused vars

--- src/screens/wallet/screen/walletScreenStyles.js
+++ src/screens/wallet/screen/walletScreenStyles.js
@@ -2,15 +2,10 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import { Dimensions } from 'react-native';
 
 const HEADER_EXPANDED_HEIGHT = 260;
-const HEADER_COLLAPSED_HEIGHT = 20;
 
 const { width: SCREEN_WIDTH } = Dimensions.get('screen');
 
 export default EStyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#eaeaea',
-  },
   scrollContainer: {
     padding: 0,
     paddingTop: HEADER_EXPANDED_HEIGHT,
@@ -22,11 +17,5 @@ export default EStyleSheet.create({
     top: 0,
     left: 0,
     zIndex: 9999,
-  },
-  title: {
-    marginVertical: 16,
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 24,
   },
 });
