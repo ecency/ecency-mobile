b317186ee8ba09ea9a1629796d26de773b2b7c62
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2019-12-09 17:36:07 +0300

    Reverted comment render system

--- src/components/commentsDisplay/view/commentsDisplayView.js
+++ src/components/commentsDisplay/view/commentsDisplayView.js
@@ -1,4 +1,4 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import { View } from 'react-native';
 import { injectIntl } from 'react-intl';
 
@@ -10,67 +10,51 @@ import COMMENT_FILTER, { VALUE } from '../../../constants/options/comment';
 // Styles
 import styles from './commentDisplayStyles';
 
-class CommentsDisplayView extends PureComponent {
-  /* Props
-   * ------------------------------------------------
-   *   @prop { type }    name                - Description....
-   */
+const CommentsDisplayView = ({
+  author,
+  commentCount,
+  fetchPost,
+  intl,
+  permlink,
+  mainAuthor,
+  handleOnVotersPress,
+}) => {
+  const [selectedFilter, setSelectedFilter] = useState(null);
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState(0);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedFilter: null,
-      selectedOptionIndex: 0,
-    };
-  }
-
-  // Component Life Cycles
-
-  // Component Functions
-  _handleOnDropdownSelect = (option, index) => {
-    this.setState({ selectedFilter: option, selectedOptionIndex: index });
+  const _handleOnDropdownSelect = (option, index) => {
+    setSelectedFilter(option);
+    setSelectedOptionIndex(index);
   };
 
-  render() {
-    const {
-      author,
-      commentCount,
-      fetchPost,
-      intl,
-      permlink,
-      mainAuthor,
-      handleOnVotersPress,
-    } = this.props;
-    const { selectedFilter, selectedOptionIndex } = this.state;
-    return (
-      <Fragment>
-        {commentCount > 0 && (
-          <Fragment>
-            <FilterBar
-              dropdownIconName="arrow-drop-down"
-              options={VALUE.map(val => intl.formatMessage({ id: `comment_filter.${val}` }))}
-              defaultText={intl.formatMessage({ id: `comment_filter.${VALUE[0]}` })}
-              onDropdownSelect={selectedIndex =>
-                this._handleOnDropdownSelect(COMMENT_FILTER[selectedIndex], selectedIndex)
-              }
-              selectedOptionIndex={selectedOptionIndex}
+  return (
+    <Fragment>
+      {commentCount > 0 && (
+        <Fragment>
+          <FilterBar
+            dropdownIconName="arrow-drop-down"
+            options={VALUE.map(val => intl.formatMessage({ id: `comment_filter.${val}` }))}
+            defaultText={intl.formatMessage({ id: `comment_filter.${VALUE[0]}` })}
+            onDropdownSelect={selectedIndex =>
+              _handleOnDropdownSelect(COMMENT_FILTER[selectedIndex], selectedIndex)
+            }
+            selectedOptionIndex={selectedOptionIndex}
+          />
+          <View style={styles.commentWrapper}>
+            <Comments
+              selectedFilter={selectedFilter}
+              fetchPost={fetchPost}
+              commentCount={commentCount}
+              author={author}
+              permlink={permlink}
+              mainAuthor={mainAuthor}
+              handleOnVotersPress={handleOnVotersPress}
             />
-            <View style={styles.commentWrapper}>
-              <Comments
-                selectedFilter={selectedFilter}
-                fetchPost={fetchPost}
-                commentCount={commentCount}
-                author={author}
-                permlink={permlink}
-                mainAuthor={mainAuthor}
-                handleOnVotersPress={handleOnVotersPress}
-              />
-            </View>
-          </Fragment>
-        )}
-      </Fragment>
-    );
-  }
-}
+          </View>
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
 
 export default injectIntl(CommentsDisplayView);
