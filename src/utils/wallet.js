674f41910e2c5587a67c30d84ac3fbff17da39c1
feruz <fmiboy@gmail.com>
2019-12-10 05:51:36 +0200

    grooming some more transaction types

--- src/utils/wallet.js
+++ src/utils/wallet.js
@@ -109,6 +109,54 @@ export const groomingTransactionData = (transaction, steemPerMVests, formatNumbe
       result.value = `${currentPays} = ${openPays}`;
       result.icon = 'reorder';
       break;
+    case 'escrow_transfer':
+    case 'escrow_dispute':
+    case 'escrow_release':
+    case 'escrow_approve':
+      const { agent, escrow_id } = opData;
+      let { from: frome } = opData;
+      let { to: toe } = opData;
+
+      result.value = `${escrow_id}`;
+      result.icon = 'wb-iridescent';
+      result.details = frome && toe ? `@${frome} to @${toe}` : null;
+      result.memo = agent || null;
+      break;
+    case 'delegate_vesting_shares':
+      const { delegator, delegatee, vesting_shares } = opData;
+
+      result.value = `${vesting_shares}`;
+      result.icon = 'change-history';
+      result.details = delegatee && delegator ? `@${delegator} to @${delegatee}` : null;
+      break;
+    case 'cancel_transfer_from_savings':
+      let { from: from_who, request_id: requestId } = opData;
+
+      result.value = `${0}`;
+      result.icon = 'cancel';
+      result.details = from_who ? `from @${from_who}, id: ${requestId}` : null;
+      break;
+    case 'fill_convert_request':
+      let { owner: who, requestid: requestedId, amount_out: amount_out } = opData;
+
+      result.value = `${amount_out}`;
+      result.icon = 'hourglass-full';
+      result.details = who ? `@${who}, id: ${requestedId}` : null;
+      break;
+    case 'fill_transfer_from_savings':
+      let { from: fillwho, to: fillto, amount: fillamount, request_id: fillrequestId } = opData;
+
+      result.value = `${fillamount}`;
+      result.icon = 'hourglass-full';
+      result.details = fillwho ? `@${fillwho} to @${fillto}, id: ${fillrequestId}` : null;
+      break;
+    case 'fill_vesting_withdraw':
+      let { from_account: pd_who, to_account: pd_to, deposited: deposited } = opData;
+
+      result.value = `${deposited}`;
+      result.icon = 'hourglass-full';
+      result.details = pd_who ? `@${pd_who} to ${pd_to}` : null;
+      break;
     default:
       return [];
   }
