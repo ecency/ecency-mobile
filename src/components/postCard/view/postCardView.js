0c0fe80aa9a4385dd627b3a2fd2e2938a57ad066
feruz <fmiboy@gmail.com>
2019-12-12 19:57:39 +0200

    clean up

--- src/components/postCard/view/postCardView.js
+++ src/components/postCard/view/postCardView.js
@@ -112,11 +112,7 @@ class PostCardView extends Component {
           </View>
         </View>
         <View style={styles.postBodyWrapper}>
-          <TouchableOpacity
-            // eslint-disable-next-line react-native/no-inline-styles
-            style={[{ flexDirection: 'column' }]}
-            onPress={this._handleOnContentPress}
-          >
+          <TouchableOpacity style={styles.hiddenImages} onPress={this._handleOnContentPress}>
             {!isHideImage && (
               <FastImage source={_image} style={styles.thumbnail} defaultSource={DEFAULT_IMAGE} />
             )}
