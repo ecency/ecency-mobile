90a2e47363f983d57170e52358ddb6d70cab183f
feruz <fmiboy@gmail.com>
2020-01-06 04:59:30 +0200

    storage import fix

--- src/realm/realm.js
+++ src/realm/realm.js
@@ -1,5 +1,5 @@
 import sha256 from 'crypto-js/sha256';
-import { AsyncStorage } from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 
 // CONSTANTS
 const USER_SCHEMA = 'user';
