513d7b726242e275f9bc371aaae0dd48b11affd1
feruz <fmiboy@gmail.com>
2020-01-08 15:19:37 +0200

    tagInput component

--- src/components/editorElements/index.js
+++ src/components/editorElements/index.js
@@ -1,5 +1,6 @@
 import SummaryArea from './summaryArea/view/summaryAreaView';
 import TagArea from './tagArea/view/tagAreaView';
 import TitleArea from './titleArea/view/titleAreaView';
+import TagInput from './tagInput/view/tagInputView';
 
-export { SummaryArea, TagArea, TitleArea };
+export { SummaryArea, TagArea, TitleArea, TagInput };
