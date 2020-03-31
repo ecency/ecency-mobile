99b5a6513d15cb2fa73266cc20ec3edd11831cee
ue <ugurerdal93@gmail.com>
2020-03-31 21:14:17 +0300

    created ui

--- src/screens/accountBoost/screen/accountBoostScreen.js
+++ src/screens/accountBoost/screen/accountBoostScreen.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { View, Platform, ScrollView } from 'react-native';
+import { View, Platform, ScrollView, Image, Text } from 'react-native';
 import get from 'lodash/get';
 import { useIntl } from 'react-intl';
 
 // Components
 import { BasicHeader, BoostPlaceHolder, ProductItemLine } from '../../../components';
 
+import LOGO_ESTM from '../../../assets/esteemcoin_boost.png';
+
 // Container
 import { InAppPurchaseContainer } from '../../../containers';
 
 // Styles
 import globalStyles from '../../../globalStyles';
+import styles from './accountBoostStyles';
 
 const ITEM_SKUS = Platform.select({
-  ios: ['999boost'],
+  ios: ['999boost', '099points'],
   android: ['999boost'],
 });
 
@@ -30,23 +33,34 @@ const AccountBoost = () => {
               id: 'boost.account.title',
             })}
           />
-
-          {isLoading ? (
-            <BoostPlaceHolder />
-          ) : (
-            <ScrollView>
-              {productList.map((product) => (
-                <ProductItemLine
-                  key={get(product, 'title')}
-                  isLoading={isLoading}
-                  disabled={isProcessing}
-                  product={product}
-                  title={get(product, 'title', '').toUpperCase()}
-                  handleOnButtonPress={(id) => buyItem(id)}
-                />
-              ))}
-            </ScrollView>
-          )}
+          <>
+            <View style={styles.container}>
+              <Image style={styles.logoEstm} source={LOGO_ESTM} />
+              <Text style={styles.desc}>
+                {intl.formatMessage({
+                  id: 'boost.account.desc',
+                })}
+              </Text>
+            </View>
+            {isLoading ? (
+              <BoostPlaceHolder />
+            ) : (
+              <ScrollView>
+                <View style={styles.productsWrapper}>
+                  {productList.map((product) => (
+                    <ProductItemLine
+                      key={get(product, 'title')}
+                      isLoading={isLoading}
+                      disabled={isProcessing}
+                      product={product}
+                      title="Boost+"
+                      handleOnButtonPress={(id) => buyItem(id)}
+                    />
+                  ))}
+                </View>
+              </ScrollView>
+            )}
+          </>
         </View>
       )}
     </InAppPurchaseContainer>
