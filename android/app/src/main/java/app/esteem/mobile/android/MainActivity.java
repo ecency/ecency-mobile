3195cda1926c4ff0422e3d1255346df20b859abd
feruz <fmiboy@gmail.com>
2020-07-16 16:52:48 +0300

    change splash

--- android/app/src/main/java/app/esteem/mobile/android/MainActivity.java
+++ android/app/src/main/java/app/esteem/mobile/android/MainActivity.java
@@ -1,12 +1,18 @@
 package app.esteem.mobile.android;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
   /**
    * Returns the name of the main component registered from JavaScript. This is
    * used to schedule rendering of the component.
@@ -22,7 +28,11 @@ public class MainActivity extends ReactActivity {
       @Override
       protected ReactRootView createRootView() {
         return new RNGestureHandlerEnabledRootView(MainActivity.this);
-      }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }      }
     };
   }
 }
