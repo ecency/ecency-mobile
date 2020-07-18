6ba5e7cc5256e6ac311d2f53de86ceeed58a4d54
feruz <fmiboy@gmail.com>
2020-07-18 22:20:33 +0300

    update dhive library7

--- src/components/upvote/view/upvoteView.js
+++ src/components/upvote/view/upvoteView.js
@@ -89,12 +89,7 @@ class UpvoteView extends Component {
       );
 
       const weight = sliderValue ? (sliderValue * 100).toFixed(0) * 100 : 0;
-      Alert.alert(
-        intl.formatMessage({
-          id: 'alert.fail',
-        }),
-        JSON.stringify({ pinCode, author, permlink, weight }),
-      );
+
       vote(currentAccount, pinCode, author, permlink, weight)
         .then(() => {
           this.setState(
