68d0d984f62d2c6e5f051fc7e7dc73fc3ebb82c4
Mustafa Buyukcelebi <mustafa.buyukcel@gmail.com>
2020-07-25 19:18:08 +0300

    Added welcome screen

---
+++ src/screens/application/screen/welcomeStyles.js
@@ -0,0 +1,33 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    paddingVertical: '5%',
+    paddingHorizontal: 40,
+  },
+  welcomeText: {
+    fontSize: 34,
+  },
+  ecencyText: {
+    fontSize: 34,
+    color: '$primaryBlue',
+  },
+  sectionRow: {
+    flexDirection: 'row',
+  },
+  sectionTitle: {
+    fontSize: 17,
+    fontWeight: '600',
+    marginLeft: 10,
+  },
+  sectionText: {
+    fontSize: 15,
+    marginLeft: 10,
+    marginRight: 45,
+  },
+  flex1: {
+    flex: 1,
+  },
+});
