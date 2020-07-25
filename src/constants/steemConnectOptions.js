065068ab1dec3d0ba80f887f2cc11b3858571a5b
feruz <fmiboy@gmail.com>
2020-07-25 15:14:04 +0300

    push hivesigner callback url change

--- src/constants/steemConnectOptions.js
+++ src/constants/steemConnectOptions.js
@@ -1,7 +1,7 @@
 export const steemConnectOptions = {
   base_url: 'https://hivesigner.com/',
   client_id: 'ecency.app',
-  redirect_uri: 'http://127.0.0.1:3415/',
+  redirect_uri: 'http://127.0.0.1:3000/auth',
   scope:
     'vote,comment,delete_comment,comment_options,custom_json,claim_reward_balance,account_update,offline',
 };
--- src/providers/steem/steemConnectAPI.js
+++ src/providers/steem/steemConnectAPI.js
@@ -2,7 +2,7 @@ import hivesigner from 'hivesigner';
 
 const api = new hivesigner.Client({
   app: 'ecency.app',
-  callbackURL: 'http://127.0.0.1:3415',
+  callbackURL: 'http://127.0.0.1:3000/auth',
 });
 
 export default api;
