a745158a4dcd107d223454097dfdc2ad425d7287
feruzm <fmiboy@gmail.com>
2020-08-08 09:08:54 +0300

    revert gesture handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import { Provider, connect } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { IntlProvider } from 'react-intl';
