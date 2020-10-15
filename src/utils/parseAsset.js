efb0356d892e9881e4ed8fdd134a5e9fb5ed8ed8
feruz <fmiboy@gmail.com>
2020-10-15 12:29:00 +0300

    fix tagcontainer leak

--- src/utils/parseAsset.js
+++ src/utils/parseAsset.js
@@ -1,6 +1,10 @@
 export default (strVal) => {
   if (typeof strVal !== 'string') {
     // console.log(strVal);
+    return {
+      amount: 0,
+      symbol: '',
+    };
   }
   const sp = strVal.split(' ');
   return {
