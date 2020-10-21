8fd3bb388793c34f7c01fba7e45dda219aee3fbd
feruz <fmiboy@gmail.com>
2020-10-21 21:16:32 +0300

    fix voters info defaults

--- src/screens/voters/screen/votersScreen.js
+++ src/screens/voters/screen/votersScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
 import { useIntl } from 'react-intl';
 import get from 'lodash/get';
+import forEach from 'lodash/forEach';
 
 // Components
 import { BasicHeader, FilterBar, VotersDisplay } from '../../../components';
@@ -11,6 +12,7 @@ import AccountListContainer from '../../../containers/accountListContainer';
 // Utils
 import { getActiveVotes } from '../../../providers/steem/dsteem';
 import { parseActiveVotes } from '../../../utils/postParser';
+import { getResizedAvatar } from '../../../utils/image';
 
 const filterOptions = ['rewards', 'percent', 'time'];
 
@@ -23,6 +25,17 @@ const VotersScreen = ({ navigation }) => {
   const headerTitle = intl.formatMessage({
     id: 'voters.voters_info',
   });
+
+  useEffect(() => {
+    const av = get(content, 'active_votes', []);
+    forEach(av, (value) => {
+      value.reward = 0;
+      value.percent = 0;
+      value.is_down_vote = Math.sign(value.rshares) < 0;
+      value.avatar = getResizedAvatar(get(value, 'voter'));
+    });
+    setActiveVotes(av);
+  }, []);
 
   useEffect(() => {
     if (content) {
