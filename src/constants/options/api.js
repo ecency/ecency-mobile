6e0a46b4a37e902dc268dc309b5d5132ad020a80
feruz <fmiboy@gmail.com>
2020-12-03 11:51:41 +0200

    server list update

--- src/constants/options/api.js
+++ src/constants/options/api.js
@@ -1,5 +1,24 @@
-export default ['rpc.esteem.app', 'api.hive.blog', 'anyx.io', 'api.pharesim.me'];
+export default [
+  'rpc.ecency.com',
+  'api.hive.blog',
+  'api.deathwing.me',
+  'anyx.io',
+  'api.pharesim.me',
+];
 
-export const VALUE = ['rpc.esteem.app', 'api.hive.blog', 'anyx.io', 'api.pharesim.me'];
+export const VALUE = [
+  'rpc.ecency.com',
+  'api.hive.blog',
+  'api.deathwing.me',
+  'anyx.io',
+  'api.pharesim.me',
+];
 
-export const SERVER_LIST = ['https://rpc.ecency.com'];
+export const SERVER_LIST = [
+  'https://rpc.ecency.com',
+  'https://api.hive.blog',
+  'https://api.deathwing.me',
+  'https://anyx.io',
+  'https://api.pharesim.me',
+  'https://rpc.ausbit.dev',
+];
