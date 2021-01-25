efbfa10ca782f475579f9fb0f76f5cfb22b13fee
feruz <fmiboy@gmail.com>
2021-01-25 22:02:48 +0200

    remove intl hook

--- src/screens/searchResult/screen/tabs/topics/view/topicsResults.js
+++ src/screens/searchResult/screen/tabs/topics/view/topicsResults.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { SafeAreaView, FlatList, View, Text, TouchableOpacity } from 'react-native';
-import { useIntl } from 'react-intl';
 
 // Components
 import { ListPlaceHolder, EmptyScreen } from '../../../../../../components/basicUIElements';
@@ -11,8 +10,6 @@ import styles from './topicsResultsStyles';
 const filterOptions = ['user', 'tag'];
 
 const TopicsResults = ({ navigation, searchValue }) => {
-  const intl = useIntl();
-
   const _renderTagItem = (item, index) => (
     <View style={[styles.itemWrapper, index % 2 !== 0 && styles.itemWrapperGray]}>
       <Text style={styles.username}>{`#${item.tag}`}</Text>
