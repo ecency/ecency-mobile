3c666ff83c0b9ffde371e08a0d807429f8604f70
Nouman Tahir <nouman.hawkdev@gmail.com>
2021-03-19 22:40:28 +0500

    updated crypto.js to avoid malfunction UTF 8 error

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,7 +1,13 @@
 import CryptoJS from 'crypto-js';
 
-export const encryptKey = (key, data) => CryptoJS.AES.encrypt(key, data).toString();
+export const encryptKey = (data, key) => {
+  const encJson = CryptoJS.AES.encrypt(JSON.stringify(data), key).toString();
+  let encData = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(encJson));
+  return encData;
+};
 
-export const decryptKey = (key, data) => {
-  return CryptoJS.AES.decrypt(key, data).toString(CryptoJS.enc.Utf8);
+export const decryptKey = (data, key) => {
+  let decData = CryptoJS.enc.Base64.parse(data).toString(CryptoJS.enc.Utf8);
+  let bytes = CryptoJS.AES.decrypt(decData, key).toString(CryptoJS.enc.Utf8);
+  return JSON.parse(bytes);
 };
