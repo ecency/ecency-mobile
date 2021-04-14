bd49d48df34d79704a72550d0a3fc3a3b112a940
Nouman Tahir <nouman.hawkdev@gmail.com>
2021-04-15 00:53:34 +0500

    welcome screen UI tweaks

--- src/screens/application/screen/welcomeStyles.js
+++ src/screens/application/screen/welcomeStyles.js
@@ -2,18 +2,16 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 import scalePx from '../../../utils/scalePx';
 
 export default EStyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-    backgroundColor: '$primaryBackgroundColor',
-  },
   container: {
-    flex: 0.95,
+    flex: 1,
     justifyContent: 'space-between',
-    paddingVertical: 40,
+    paddingVertical: 100,
     paddingHorizontal: 40,
+    backgroundColor: '$primaryBackgroundColor',
   },
   welcomeText: {
     fontSize: scalePx(34),
+    color: '$primaryBlack',
   },
   ecencyText: {
     fontSize: scalePx(34),
@@ -23,25 +21,33 @@ export default EStyleSheet.create({
     position: 'absolute',
     width: 160,
     height: 227,
-    marginTop: 40,
+    marginTop: 90,
     right: 0,
-    opacity: 0.5,
+    opacity: 0.7,
+    shadowColor: '$primaryBlack',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.2,
+    shadowRadius: 20,
+    overflow: 'visible',
   },
   topText: {
     marginTop: 40,
   },
   sectionRow: {
     flexDirection: 'row',
+    marginTop: 64,
   },
   sectionTitle: {
     fontSize: scalePx(17),
     fontWeight: '600',
     marginLeft: 10,
+    color: '$primaryBlack',
   },
   sectionText: {
     fontSize: scalePx(15),
     marginLeft: 10,
     marginRight: 45,
+    color: '$primaryBlack',
   },
   flex1: {
     flex: 1,
