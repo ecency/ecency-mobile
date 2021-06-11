b3b8b3d66c4171accd7c3d29a4363edfc080c355
Nouman Tahir <nouman.hawkdev@gmail.com>
2021-06-11 22:51:23 +0500

    webview conversion to functional component

--- src/screens/webBrowser/webBrowser.tsx
+++ src/screens/webBrowser/webBrowser.tsx
@@ -1,47 +1,45 @@
-import React, { PureComponent } from 'react';
-import { View, StatusBar } from 'react-native';
+import React, {useMemo } from 'react';
+import {Alert, StatusBar } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-import { injectIntl } from 'react-intl';
-import { withNavigation } from 'react-navigation';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { BasicHeader } from '../../components';
+import {get} from 'lodash';
 
-class WebBrowser extends PureComponent {
-  constructor(props) {
-    super(props);
+export interface WebBrowserParams {
+    url:string;
+}
 
-    this.state = {
-      isLoading: false,
-    };
-  }
-
-  _onNavigationStateChange = (event) => {
-
-  };
-
-  render() {
-    const url = this.props.navigation.getParam('url');
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar />
-        <BasicHeader
-          title="Ecency"
-        />
-        <WebView
-          source={{
-            uri: url,
-          }}
-          onNavigationStateChange={this._onNavigationStateChange}
-          ref={(ref) => {
-            this.webview = ref;
-          }}
-        />
-      </SafeAreaView>
-    );
+interface Props {
+  navigation:{
+    state:{
+      params:WebBrowserParams
+    }
   }
 }
 
+const WebBrowser = (props:Props) => {
+
+  const url = useMemo(() => get(props, 'navigation.state.params.url'), []);
+
+  if(!url){
+    Alert.alert("DEV: url parameter cannot be empty")
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar />
+      <BasicHeader
+        title="Ecency"
+      />
+      <WebView
+        source={{
+          uri: url,
+        }}
+      />
+    </SafeAreaView>
+  )
+}
 
 
-export default injectIntl(withNavigation(WebBrowser));
+export default WebBrowser
