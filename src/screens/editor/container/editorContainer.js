00b3528eae71e1e5a81fcf30262f380eaa598c1a
Nouman Tahir <noumantahir@live.com>
2021-07-09 15:40:43 +0500

    fixed isPostSending undefined bug

--- src/screens/editor/container/editorContainer.js
+++ src/screens/editor/container/editorContainer.js
@@ -674,7 +674,7 @@ class EditorContainer extends Component {
 
   _submitReply = async (fields) => {
     const { currentAccount, pinCode } = this.props;
-    const { rewardType, beneficiaries } = this.state;
+    const { rewardType, beneficiaries, isPostSending } = this.state;
 
     if (isPostSending) {
       return;
@@ -720,11 +720,12 @@ class EditorContainer extends Component {
 
   _submitEdit = async (fields) => {
     const { currentAccount, pinCode } = this.props;
-    const { post, isEdit } = this.state;
+    const { post, isEdit, isPostSending } = this.state;
 
     if (isPostSending) {
       return;
     }
+
 
     if (currentAccount) {
       this.setState({
