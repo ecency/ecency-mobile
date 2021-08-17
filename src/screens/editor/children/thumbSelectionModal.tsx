3bc48fcb161c7b45aba21009a523dcd82849b992
Nouman Tahir <nouman.hawkdev@gmail.com>
2021-08-17 20:54:19 +0500

    Introduced thumbnail selection modal

---
+++ src/screens/editor/children/thumbSelectionModal.tsx
@@ -0,0 +1,98 @@
+import React, { useImperativeHandle, useRef, useState } from 'react';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import ActionSheet from 'react-native-actions-sheet';
+import EStyleSheet from 'react-native-extended-stylesheet';
+import styles from './thumbSelectionModalStyles';
+import { extractImageUrls } from '../../../utils/editor';
+import FastImage from 'react-native-fast-image';
+import { forwardRef } from 'react';
+import { View, Text } from 'react-native';
+import { useIntl } from 'react-intl';
+
+export interface ThumbSelectionModalProps {
+  onThumbSelection:(index:number)=>void,
+}
+
+
+const ThumbSelectionModal = ({ onThumbSelection }:ThumbSelectionModalProps, ref) => {
+  const intl = useIntl();
+
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+  const sheetModalRef = useRef<ActionSheet>();
+
+  //CALLBACK_METHODS
+  useImperativeHandle(ref, () => ({
+      show: (postBody:string) => {
+          console.log("Showing action modal")
+
+          const urls = extractImageUrls({body:postBody});
+
+          if(urls.length < 2){
+              console.log("Skipping modal show as post images are less than 2");
+              onThumbSelection(0);
+              return;
+          }
+
+          setImageUrls(urls);
+          sheetModalRef.current?.setModalVisible(true);
+      }
+  }));
+
+
+  const _onSelection = (index:number) => {
+    onThumbSelection(index);
+    sheetModalRef.current?.setModalVisible(false);
+  }
+
+
+
+  //VIEW_RENDERERS
+  const _renderImageItem = ({item, index}:{item:string, index:number}) => {
+      const _onPress = () => {
+          _onSelection(index);
+      }
+
+    return (
+        <TouchableOpacity onPress={_onPress} >
+            <FastImage 
+                source={{uri:item}}
+                style={styles.thumbStyle}
+                resizeMode='cover'
+            />
+        </TouchableOpacity>
+    )
+  }
+
+
+  const _renderContent = () => {
+      return (
+        <View style={{alignItems:'center'}} >
+            <Text style={styles.title}>{intl.formatMessage({id:'editor.select_thumb'})}</Text>
+            <FlatList
+              data={imageUrls}
+              renderItem={_renderImageItem}
+              keyExtractor={(item, index)=>item.url + index}
+              horizontal={true}
+              contentContainerStyle={styles.listContainer}
+              showsHorizontalScrollIndicator={false}
+          />
+        </View>
+        
+      )
+  }
+
+
+  return (
+    <ActionSheet 
+     ref={sheetModalRef}
+     gestureEnabled={false}
+     hideUnderlay
+     containerStyle={styles.sheetContent}
+     indicatorColor={EStyleSheet.value('$primaryWhiteLightBackground')}
+     > 
+     {_renderContent()}
+ </ActionSheet> 
+  );
+};
+
+export default forwardRef(ThumbSelectionModal);
---
+++ src/screens/editor/children/thumbSelectionModalStyles.ts
@@ -0,0 +1,32 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+export default EStyleSheet.create({
+
+  sheetContent: {
+    backgroundColor: '$primaryBackgroundColor',
+    position:'absolute',
+    bottom:0,
+    left:0, 
+    right:0,
+    zIndex:999
+  },
+  thumbStyle:{
+      width:100, 
+      height:100,
+      margin:8,
+      borderRadius:12,
+      backgroundColor:'$primaryLightGray'
+    },
+    title:{
+      color: '$primaryBlack',
+      fontWeight: 'bold',
+      fontSize: 18,
+      padding: 16,
+      textAlign:'center'
+    },
+    listContainer:{
+      paddingHorizontal:8,  
+      paddingBottom:getBottomSpace() + 16,
+    }
+});
