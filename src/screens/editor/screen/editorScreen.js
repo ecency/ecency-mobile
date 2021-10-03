64f364b605786d24034d0b31f9cab29a5a11c7b1
noumantahir <nouman.hawkdev@gmail.com>
2021-10-03 12:01:13 +0500

    form validity fix
    total of 10 tags are allowed but publish button was being disabled at 9 because of wrong comparison

--- src/screens/editor/screen/editorScreen.js
+++ src/screens/editor/screen/editorScreen.js
@@ -199,7 +199,7 @@ class EditorScreen extends Component {
         get(fields, 'title', '').length < 255 &&
         (get(fields, 'body', '') || (bodyText && bodyText > 0)) &&
         get(fields, 'tags', null) &&
-        get(fields, 'tags', null).length < 10 &&
+        get(fields, 'tags', null).length <= 10 &&
         isLoggedIn;
     }
     this.setState({ isFormValid });
