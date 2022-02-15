176fa12a01cdb0c90694116f8361ac33582c8bb8
Sadaqat Ali <sali.bscs17seecs@seecs.edu.pk>
2022-02-15 10:03:24 +0500

    added player controls and fixed on android

--- src/components/videoPlayer/videoPlayerView.tsx
+++ src/components/videoPlayer/videoPlayerView.tsx
@@ -4,6 +4,7 @@ import { View, StyleSheet, ActivityIndicator } from 'react-native';
 import WebView from 'react-native-webview';
 import YoutubeIframe, { InitialPlayerParams } from 'react-native-youtube-iframe';
 import Video from 'react-native-video';
+import MediaControls, { PLAYER_STATES } from 'react-native-media-controls';
 
 interface VideoPlayerProps {
   mode: 'uri' | 'youtube';
@@ -27,11 +28,19 @@ const VideoPlayer = ({
   const checkSrcRegex = /(.*?)\.(mp4|webm|ogg)$/gi;
   const isExtensionType = mode === 'uri' ? uri.match(checkSrcRegex) : false;
 
+  const videoPlayer = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [paused, setPaused] = useState(true);
+  const [playerState, setPlayerState] = useState(PLAYER_STATES.PAUSED);
+  const [screenType, setScreenType] = useState('contain');
+
+  // react-native-youtube-iframe handlers
   const [shouldPlay, setShouldPlay] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const videoPlayerRef = useRef(null);
   const _onReady = () => {
-    setLoading(false);
+    setIsLoading(false);
     setShouldPlay(disableAutoplay ? false : true);
     console.log('ready');
   };
@@ -43,13 +52,101 @@ const VideoPlayer = ({
 
   const _onError = () => {
     console.log('error!');
-    setLoading(false);
+    setIsLoading(false);
   };
 
   const initialParams: InitialPlayerParams = {
     start: startTime,
   };
 
+  // react-native-video player handlers
+  const onSeek = (seek) => {
+    videoPlayer.current.seek(seek);
+  };
+
+  const onPaused = (playerState) => {
+    setPaused(!paused);
+    setPlayerState(playerState);
+  };
+
+  const onReplay = () => {
+    setPlayerState(PLAYER_STATES.PLAYING);
+    videoPlayer.current.seek(0);
+  };
+
+  const onProgress = (data) => {
+    if (!isLoading && playerState !== PLAYER_STATES.ENDED) {
+      setCurrentTime(data.currentTime);
+    }
+  };
+
+  const onLoad = (data) => {
+    setDuration(data.duration);
+    videoPlayer.current.seek(0);
+    setIsLoading(false);
+  };
+
+  const onLoadStart = () => setIsLoading(true);
+
+  const onEnd = () => setPlayerState(PLAYER_STATES.ENDED);
+
+  const onError = () => alert('Error while playing');
+
+  const exitFullScreen = () => {
+    setIsFullScreen(false);
+  };
+
+  const enterFullScreen = () => {
+    setIsFullScreen(true);
+  };
+
+  const onFullScreen = () => {
+    setIsFullScreen(true);
+    if (screenType == 'contain') setScreenType('cover');
+    else setScreenType('contain');
+  };
+
+  const onSeeking = (currentTime) => setCurrentTime(currentTime);
+
+  const _renderVideoplayerWithControls = () => {
+    return (
+      <View style={{ flex: 1 }}>
+        <Video
+          source={{
+            uri: uri,
+          }}
+          onEnd={onEnd}
+          onLoad={onLoad}
+          onLoadStart={onLoadStart}
+          onProgress={onProgress}
+          onError={onError}
+          paused={paused}
+          ref={videoPlayer}
+          resizeMode={'cover'}
+          fullscreen={isFullScreen}
+          style={styles.mediaPlayer}
+          volume={10}
+          onFullscreenPlayerDidPresent={enterFullScreen}
+          onFullscreenPlayerDidDismiss={exitFullScreen}
+        />
+        <MediaControls
+          duration={duration}
+          isLoading={isLoading}
+          mainColor="#357ce6"
+          onFullScreen={onFullScreen}
+          onPaused={onPaused}
+          onReplay={onReplay}
+          onSeek={onSeek}
+          onSeeking={onSeeking}
+          playerState={playerState}
+          progress={currentTime}
+          isFullScreen={isFullScreen}
+          fadeOutDelay={30000}
+          containerStyle={{}}
+        />
+      </View>
+    );
+  };
   return (
     <View style={styles.container}>
       {mode === 'youtube' && youtubeVideoId && (
@@ -66,20 +163,9 @@ const VideoPlayer = ({
         </View>
       )}
       {mode === 'uri' && uri && (
-        <View style={{ height: PLAYER_HEIGHT }}>
+        <View style={[styles.playerWrapper, { height: PLAYER_HEIGHT }]}>
           {isExtensionType ? (
-            <Video
-              source={{ uri: uri }}
-              ref={videoPlayerRef}
-              onBuffer={() => console.log('buffering')}
-              onError={() => console.log('error while playing')}
-              onEnd={() => console.log('end playing!')}
-              onLoadStart={() => setLoading(true)}
-              onLoad={() => setLoading(false)}
-              style={styles.videoPlayer}
-              controls
-              paused
-            />
+            _renderVideoplayerWithControls()
           ) : (
             <WebView
               scalesPageToFit={true}
@@ -87,13 +173,13 @@ const VideoPlayer = ({
               javaScriptEnabled={true}
               automaticallyAdjustContentInsets={false}
               onLoadEnd={() => {
-                setLoading(false);
+                setIsLoading(false);
               }}
               onLoadStart={() => {
-                setLoading(true);
+                setIsLoading(true);
               }}
               source={{ uri: uri }}
-              style={{ width: contentWidth, height: PLAYER_HEIGHT }}
+              style={[styles.barkBackground, { width: contentWidth, height: PLAYER_HEIGHT }]}
               startInLoadingState={true}
               onShouldStartLoadWithRequest={() => true}
               mediaPlaybackRequiresUserAction={true}
@@ -102,7 +188,9 @@ const VideoPlayer = ({
           )}
         </View>
       )}
-      {loading && <ActivityIndicator style={styles.activityIndicator} />}
+      {isLoading && (
+        <ActivityIndicator size={'large'} color="white" style={styles.activityIndicator} />
+      )}
     </View>
   );
 };
@@ -117,16 +205,30 @@ const styles = StyleSheet.create({
     position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
-    top: 0,
+    top: 25,
     bottom: 0,
     left: 0,
     right: 0,
   },
-  videoPlayer: {
+  toolbar: {
+    marginTop: 30,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  playerWrapper: {
+    backgroundColor: 'black',
+  },
+  mediaPlayer: {
     position: 'absolute',
     top: 0,
-    bottom: 0,
     left: 0,
+    bottom: 0,
     right: 0,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+  },
+  barkBackground: {
+    backgroundColor: 'black',
   },
 });
