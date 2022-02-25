0ce0fc3299dde1ba2920288038382c00a1bda700
Sadaqat Ali <sali.bscs17seecs@seecs.edu.pk>
2022-02-25 09:55:27 +0500

    added basic qr scanner

--- src/screens/qrScreen/qrScreen.tsx
+++ src/screens/qrScreen/qrScreen.tsx
@@ -1,15 +1,51 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 import { useIntl } from 'react-intl';
-import { View, Text } from 'react-native';
-import { BasicHeader } from '../../components';
+import { View, Text, ScrollView, Button, TouchableOpacity, Dimensions } from 'react-native';
+import { BasicHeader, MainButton } from '../../components';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 // styles
 import styles from './qrScreenStyle';
 
 //
+const screenWidth = Dimensions.get('screen').width;
 const QRScreen = () => {
   const intl = useIntl();
+  const [isActive, setIsActive] = useState(true);
+  const [scanResult, setScanResult] = useState(null);
+  const scannerRef = useRef(null);
+  const onSuccess = (e) => {
+    setScanResult(e.data);
+    setIsActive(false);
+  };
 
+  const _handleOpen = () => {
+    console.log('scanResult : ', scanResult);
+  };
+  const _renderBottomContent = () => {
+    return (
+      <View style={styles.bottomContent}>
+        <Text>Detected URL:</Text>
+        <Text>{scanResult}</Text>
+        
+        <MainButton
+          // isLoading={isLoading}
+          isDisable={isActive}
+          style={styles.mainButton}
+          height={50}
+          onPress={_handleOpen}
+        >
+          <View style={styles.mainButtonWrapper}>
+            <Text style={styles.openText}>
+              {intl.formatMessage({
+                id: 'qr.open',
+              })}
+            </Text>
+          </View>
+        </MainButton>
+      </View>
+    );
+  };
   return (
     <Fragment>
       <BasicHeader
@@ -18,7 +54,18 @@ const QRScreen = () => {
         })}
       />
       <View style={styles.mainContainer}>
-        <Text>QR Screen</Text>
+        <QRCodeScanner
+          reactivate={isActive}
+          showMarker={true}
+          ref={scannerRef}
+          onRead={onSuccess}
+          topViewStyle={{ display: 'none' }}
+          bottomContent={_renderBottomContent()}
+          containerStyle={styles.scannerContainer}
+          cameraContainerStyle={styles.cameraContainer}
+          cameraStyle={{ alignSelf: 'center', justifyContent: 'center', width: 200 }}
+          cameraProps={{}}
+        />
       </View>
     </Fragment>
   );
--- src/screens/qrScreen/qrScreenStyle.ts
+++ src/screens/qrScreen/qrScreenStyle.ts
@@ -5,4 +5,30 @@ export default EStyleSheet.create({
     flex: 1,
     backgroundColor: '$primaryBackgroundColor',
   },
+
+  bottomContent: {
+    flex: 1,
+    width: '100%',
+  },
+  scannerContainer: {
+    flex: 1,
+  },
+  cameraContainer: {
+    backgroundColor: 'black',
+  },
+  mainButton: {
+    marginTop: 16,
+    alignSelf: 'center',
+    paddingHorizontal: 24,
+  },
+  mainButtonWrapper: {
+    flexDirection: 'row',
+  },
+  openText: {
+    color: '$pureWhite',
+    fontSize: 14,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
 });
