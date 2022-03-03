f4b1eaff1bd168292ecc3bd56ebe1a68b7e1d807
Sadaqat Ali <sali.bscs17seecs@seecs.edu.pk>
2022-03-03 22:03:44 +0500

    added qr scanner

--- src/components/qrModal/qrModalStyles.ts
+++ src/components/qrModal/qrModalStyles.ts
@@ -2,16 +2,55 @@ import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
   sheetContent: {
-    backgroundColor: '$primaryBackgroundColor',
+    backgroundColor: '$black',
     position: 'absolute',
     bottom: 0,
     left: 0,
     right: 0,
     zIndex: 999,
   },
-  container:{
-    flex:1,
+  container: {
+    flex: 1,
   },
-  
-  
+  mainContainer: {
+    backgroundColor: '$black',
+    height: '100%',
+  },
+  scannerContainer: {
+    backgroundColor: '$black',
+  },
+  cameraContainer: {
+    backgroundColor: '$black',
+  },
+  mainButton: {
+    marginTop: 16,
+    alignSelf: 'center',
+    paddingHorizontal: 24,
+  },
+  mainButtonWrapper: {
+    flexDirection: 'row',
+  },
+  openText: {
+    color: '$pureWhite',
+    fontSize: 14,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+  urlContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 16,
+  },
+  urlTextContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  validIcon: {
+    width: 30,
+  },
+  cameraStyle: {
+    width: '100%',
+    height: '100%',
+  },
 });
--- src/components/qrModal/qrModalView.tsx
+++ src/components/qrModal/qrModalView.tsx
@@ -1,18 +1,33 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Text, View } from 'react-native';
 import ActionSheet from 'react-native-actions-sheet';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import styles from './qrModalStyles';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { toggleQRModal } from '../../redux/actions/uiAction';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { deepLinkParser } from '../../utils/deepLinkParser';
+import { useIntl } from 'react-intl';
+import { navigate } from '../../navigation/service';
+import { Icon, MainButton } from '..';
+import { Dimensions } from 'react-native';
 
 export interface QRModalProps {}
 
+const screenHeight = Dimensions.get("window").height
 export const QRModal = ({}: QRModalProps) => {
-  const sheetModalRef = useRef<ActionSheet>();
   const dispatch = useAppDispatch();
-    
+  const intl = useIntl();
   const isVisibleQRModal = useAppSelector((state) => state.ui.isVisibleQRModal);
+  const currentAccount = useAppSelector((state) => state.account.currentAccount);
+
+  const [isActive, setIsActive] = useState(true);
+  const [scanURL, setScanURL] = useState(null);
+  const [navData, setNavData] = useState(null);
+  const [isURLValid, setIsURLValid] = useState(false);
+
+  const sheetModalRef = useRef<ActionSheet>();
+  const scannerRef = useRef(null);
 
   useEffect(() => {
     if (isVisibleQRModal) {
@@ -26,19 +41,95 @@ export const QRModal = ({}: QRModalProps) => {
     dispatch(toggleQRModal(false));
   };
 
+  const onSuccess = (e) => {
+    _handleDeepLink(e.data);
+    setIsActive(false);
+  };
+
+  const _handleDeepLink = async (url) => {
+    const deepLinkData = await deepLinkParser(url, currentAccount);
+    const { routeName, params, key } = deepLinkData || {};
+    setNavData(routeName && params && key ? deepLinkData : null);
+    setIsURLValid(routeName && params && key ? true : false);
+    setScanURL(url);
+  };
+
+  const _handleOpen = () => {
+    if (navData) {
+      _onClose()
+      navigate(navData);
+    }
+  };
+  
+  const _renderBottomContent = () => {
+    return (
+      <View style={styles.bottomContent}>
+        <View style={styles.urlContainer}>
+          <View style={styles.urlTextContainer}>
+            <Text>
+              {intl.formatMessage({
+                id: 'qr.detected_url',
+              })}
+              :
+            </Text>
+            <Text>{scanURL}</Text>
+          </View>
+          <View style={styles.validIcon}>
+            {scanURL && (
+              <Icon
+                iconType="Ionicons"
+                name={isURLValid ? 'checkmark-circle-outline' : 'close-circle-outline'}
+                size={20}
+                color={isURLValid ? 'green' : 'red'}
+              />
+            )}
+          </View>
+        </View>
+        <MainButton
+          // isLoading={isLoading}
+          isDisable={isActive || !isURLValid}
+          style={styles.mainButton}
+          height={50}
+          onPress={_handleOpen}
+        >
+          <View style={styles.mainButtonWrapper}>
+            <Text style={styles.openText}>
+              {intl.formatMessage({
+                id: 'qr.open',
+              })}
+            </Text>
+          </View>
+        </MainButton>
+      </View>
+    );
+  };
+
   return (
     <ActionSheet
       ref={sheetModalRef}
       gestureEnabled={true}
-      containerStyle={styles.sheetContent}
+      containerStyle={{...styles.sheetContent, height: screenHeight }}
       onClose={_onClose}
       indicatorColor={EStyleSheet.value('$primaryWhiteLightBackground')}
+   
     >
-      <View>
-        <Text>QR Modal</Text>
+      <View style={styles.mainContainer}>
+      {/* <Text>QR Modal</Text> */}
+
+        <QRCodeScanner
+          reactivate={isActive}
+          showMarker={true}
+          ref={scannerRef}
+          onRead={onSuccess}
+          topViewStyle={{ display: 'none' }}
+          bottomViewStyle={{display:'none'}}
+          containerStyle={styles.scannerContainer}
+          cameraContainerStyle={styles.cameraContainer}
+          cameraStyle={styles.cameraStyle}
+        />
       </View>
     </ActionSheet>
   );
 };
 
-export default QRModal
+export default QRModal;
