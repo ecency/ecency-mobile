c220f677981c72065404b1710454848017a4295d
noumantahir <nouman.hawkdev@gmail.com>
2022-05-02 02:11:41 +0500

    added loading indicator on claim button

--- src/screens/wallet/screen/walletScreen.tsx
+++ src/screens/wallet/screen/walletScreen.tsx
@@ -232,6 +232,7 @@ const WalletScreen = ({navigation}) => {
         unclaimedRewards={coinData.unclaimedBalance}
         enableBuy={!coinData.unclaimedBalance && item.id === COIN_IDS.ECENCY}
         isClaiming={isClaiming}
+        isLoading={isLoading}
         onCardPress={_onCardPress}
         onClaimPress={_onClaimPress}
         footerComponent={index === 0 && <HorizontalIconList options={POINTS} optionsKeys={POINTS_KEYS} />}
