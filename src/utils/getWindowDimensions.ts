5af65594750620aae60fb212afde96234df223bf
Sadaqat Ali <sali.bscs17seecs@seecs.edu.pk>
2022-05-28 06:23:41 +0500

    changed import statement and method implementation a little bit

--- src/utils/getWindowDimensions.ts
+++ src/utils/getWindowDimensions.ts
@@ -1,10 +1,11 @@
 import { Dimensions } from 'react-native';
-import {getOrientation, getInitialOrientation} from 'react-native-orientation-locker';
+import Orientation from 'react-native-orientation-locker';
 import { orientations } from '../redux/constants/orientationsConstants';
 
 
 const getWindowDimensions = () => {
-  const orientation = getOrientation ? getOrientation() : getInitialOrientation();
+  const {getOrientation, getInitialOrientation} = Orientation;
+  const orientation =  getInitialOrientation();
   const isDeviceRotated = orientation !== orientations.PORTRAIT;
 
   const width = isDeviceRotated
