79e15141adea00d51998ceeb13af7996c2d22b72
noumantahir <nouman.hawkdev@gmail.com>
2022-06-17 18:32:43 +0500

    remove application store from blacklist

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -5,7 +5,6 @@ import AsyncStorage from '@react-native-community/async-storage';
 import Reactotron from '../../../reactotron-config';
 
 import reducer from '../reducers';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const transformCacheVoteMap = createTransform(
   (inboundState:any) => ({ 
@@ -38,7 +37,7 @@ const persistConfig = {
   // Storage Method (React Native)
   storage: AsyncStorage,
   // Blacklist (Don't Save Specific Reducers)
-  blacklist: ['nav', 'application', 'communities', 'user'],
+  blacklist: ['nav', 'communities', 'user'],
   timeout: 0,
   transforms:[
     transformCacheVoteMap,
