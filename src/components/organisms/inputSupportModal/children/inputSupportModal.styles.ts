02ece3d2def4f3483d1ac50bfcdafadac2feeb10
noumantahir <nouman.hawkdev@gmail.com>
2022-06-24 18:05:25 +0500

    added inputSupportModal for generic use

---
+++ src/components/organisms/inputSupportModal/children/inputSupportModal.styles.ts
@@ -0,0 +1,12 @@
+import {ViewStyle } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+export default EStyleSheet.create({
+
+    container: {
+        flex: 1,
+        justifyContent:"flex-end",
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+      } as ViewStyle,
+
+})
