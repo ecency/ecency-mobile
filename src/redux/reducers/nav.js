e1476d93b9b5906f3b826a41f2ae13ae99f90238
Nouman Tahir <nouman.hawkdev@gmail.com>
2022-08-07 17:53:30 +0500

    added pending todo

--- src/redux/reducers/nav.js
+++ src/redux/reducers/nav.js
@@ -1,5 +1,5 @@
 // import Navigator from '../../navigation/appNavigator';
-
+//TOOD: put back state for action
 export default (state, action) => {
   // const newState = Navigator.router.getStateForAction(action, state);
   return {}; //newState || state;
