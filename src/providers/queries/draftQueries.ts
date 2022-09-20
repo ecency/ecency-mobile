d45c6c29a8ea862dec9dc73de338397f059aaf04
Nouman Tahir <nouman.hawkdev@gmail.com>
2022-09-20 13:53:29 +0500

    added basic queries structure

---
+++ src/providers/queries/draftQueries.ts
@@ -0,0 +1,47 @@
+import { useQuery } from '@tanstack/react-query';
+import { getDrafts, getSchedules } from '../ecency/ecency';
+import QUERIES from './queries';
+
+/** hook used to return user drafts */
+export const useGetDraftsQuery = () => {
+  return useQuery([QUERIES.DRAFTS.GET], _getDrafts);
+};
+
+/** used to return user schedules */
+export const useGetSchedulesQuery = () => {
+  return useQuery([QUERIES.SCHEDULES.GET], _getSchedules);
+};
+
+const _getDrafts = async () => {
+  try {
+    const data = await getDrafts();
+    return _sortData(data || []);
+  } catch (err) {
+    throw new Error('draft.load_error');
+  }
+};
+
+const _getSchedules = async () => {
+  try {
+    const data = await getSchedules();
+    return _sortDataS(data);
+  } catch (err) {
+    throw new Error('drafts.load_error');
+  }
+};
+
+const _sortDataS = (data) =>
+  data.sort((a, b) => {
+    const dateA = new Date(a.schedule).getTime();
+    const dateB = new Date(b.schedule).getTime();
+
+    return dateB > dateA ? 1 : -1;
+  });
+
+const _sortData = (data) =>
+  data.sort((a, b) => {
+    const dateA = new Date(a.created).getTime();
+    const dateB = new Date(b.created).getTime();
+
+    return dateB > dateA ? 1 : -1;
+  });
