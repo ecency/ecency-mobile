c8d566bb727e7a69bfdae3951d859c51ff4ff915
Nouman Tahir <nouman.hawkdev@gmail.com>
2022-09-20 17:10:01 +0500

    support for persisted query client

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { IntlProvider } from 'react-intl';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Host } from 'react-native-portalize';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
 import { flattenMessages } from './utils/flattenMessages';
 import messages from './config/locales';
 
@@ -13,10 +13,10 @@ import Application from './screens/application';
 import { store, persistor } from './redux/store/store';
 import { initQueryClient } from './providers/queries';
 
-const queryClient = initQueryClient();
+const queryClientProviderProps = initQueryClient();
 
 const _renderApp = ({ locale }) => (
-  <QueryClientProvider client={queryClient}>
+  <PersistQueryClientProvider {...queryClientProviderProps}>
     <PersistGate loading={null} persistor={persistor}>
       <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
         <SafeAreaProvider>
@@ -26,7 +26,7 @@ const _renderApp = ({ locale }) => (
         </SafeAreaProvider>
       </IntlProvider>
     </PersistGate>
-  </QueryClientProvider>
+  </PersistQueryClientProvider>
 );
 
 const mapStateToProps = (state) => ({
