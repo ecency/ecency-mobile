92814d05f32ec52aaa75dd59ea2c3e096df001f2
Sadaqat Ali <48380998+aliseyalvi@users.noreply.github.com>
2022-09-28 11:50:02 +0500

    ignore require cycle and debugger warnings with LogBox

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 import AppCenter from 'appcenter';
 import { name as appName } from './app.json';
 import 'core-js';
@@ -9,5 +9,8 @@ import 'intl/locale-data/jsonp/en-US';
 const EcencyApp = require('./App').default;
 
 AppCenter.setLogLevel(AppCenter.LogLevel.VERBOSE);
+
+// ignore warnings
+LogBox.ignoreLogs(['Require cycle:', 'Remote debugger']);
 
 AppRegistry.registerComponent(appName, () => EcencyApp);
