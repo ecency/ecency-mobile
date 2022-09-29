9816439355868c3ec431812882ce5ae9c6c774c6
noumantahir <nouman.hawkdev@gmail.com>
2022-09-29 14:25:49 +0500

    added issue reference

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import AppCenter from 'appcenter';
 import { name as appName } from './app.json';
 import 'core-js';
 import 'intl';
-import 'intl/locale-data/jsonp/en-US';
+import 'intl/locale-data/jsonp/en-US'
 
 // set check frequency options
 const EcencyApp = require('./App').default;
 
 AppCenter.setLogLevel(AppCenter.LogLevel.VERBOSE);
 
+
+// TODO Remove ignoreLogs when referenced issue is fixed properly
+// ref: https://github.com/ecency/ecency-mobile/issues/2466
 // ignore warnings
 LogBox.ignoreLogs(['Require cycle:', 'Remote debugger']);
 
