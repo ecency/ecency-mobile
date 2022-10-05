6291b92bb288d1085eb92debd2c999fd2691acd3
Nouman Tahir <nouman.hawkdev@gmail.com>
2022-10-05 18:55:24 +0500

    lint

--- src/providers/queries/editorQueries.ts
+++ src/providers/queries/editorQueries.ts
@@ -1,18 +1,16 @@
-import { useQuery } from "@tanstack/react-query"
-import { useIntl } from "react-intl"
-import { useAppDispatch } from "../../hooks"
-import { toastNotification } from "../../redux/actions/uiAction"
-import { getFragments } from "../ecency/ecency"
-import QUERIES from "./queryKeys"
-
-
+import { useQuery } from '@tanstack/react-query';
+import { useIntl } from 'react-intl';
+import { useAppDispatch } from '../../hooks';
+import { toastNotification } from '../../redux/actions/uiAction';
+import { getFragments } from '../ecency/ecency';
+import QUERIES from './queryKeys';
 
 export const useSnippetsQuery = () => {
-    const intl = useIntl();
-    const dispatch = useAppDispatch();
-    return useQuery([QUERIES.SNIPPETS.GET], getFragments, {
-        onError:()=>{
-            dispatch(toastNotification(intl.formatMessage({ id: 'alert.fail' })));
-        }
-    })
-}
+  const intl = useIntl();
+  const dispatch = useAppDispatch();
+  return useQuery([QUERIES.SNIPPETS.GET], getFragments, {
+    onError: () => {
+      dispatch(toastNotification(intl.formatMessage({ id: 'alert.fail' })));
+    },
+  });
+};
