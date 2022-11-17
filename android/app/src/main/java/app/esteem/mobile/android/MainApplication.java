5271d1fa452a1d0ad3aea6b3f255354dce9ce958
Nouman Tahir <noumantahir@live.com>
2022-11-17 12:00:37 +0500

    enabled flipper on android

--- android/app/src/main/java/app/esteem/mobile/android/MainApplication.java
+++ android/app/src/main/java/app/esteem/mobile/android/MainApplication.java
@@ -81,8 +81,40 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
     // If you opted-in for the New Architecture, we enable the TurboModule system
     ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     SoLoader.init(this, /* native exopackage */ false);
+    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
     // Uncomment below line to Debug Webview
     // WebView.setWebContentsDebuggingEnabled(true);
     registerActivityLifecycleCallbacks(OrientationActivityLifecycle.getInstance());
+  }
+
+  /**
+   * Loads Flipper in React Native templates. Call this in the onCreate method with something like
+   * initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+   *
+   * @param context
+   * @param reactInstanceManager
+   */
+  private static void initializeFlipper(
+          Context context, ReactInstanceManager reactInstanceManager) {
+    if (BuildConfig.DEBUG) {
+      try {
+        /*
+         We use reflection here to pick up the class that initializes Flipper,
+        since Flipper library is not available in release mode
+        */
+        Class<?> aClass = Class.forName("com.sample.ReactNativeFlipper");
+        aClass
+                .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+                .invoke(null, context, reactInstanceManager);
+      } catch (ClassNotFoundException e) {
+        e.printStackTrace();
+      } catch (NoSuchMethodException e) {
+        e.printStackTrace();
+      } catch (IllegalAccessException e) {
+        e.printStackTrace();
+      } catch (InvocationTargetException e) {
+        e.printStackTrace();
+      }
+    }
   }
 }
