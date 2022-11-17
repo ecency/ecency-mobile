9e847535c29fabc97c7a950b27268f1f7fbfdc2f
Nouman Tahir <noumantahir@live.com>
2022-11-17 15:20:57 +0500

    updated flipper config, integrated reatotron and redux plugins

--- android/app/src/main/java/app/esteem/mobile/android/MainApplication.java
+++ android/app/src/main/java/app/esteem/mobile/android/MainApplication.java
@@ -102,7 +102,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.sample.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("app.esteem.mobile.android.ReactNativeFlipper");
         aClass
                 .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
                 .invoke(null, context, reactInstanceManager);
