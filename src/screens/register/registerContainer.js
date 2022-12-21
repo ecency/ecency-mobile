7184f3c5602e52d060d0fbdc4d59fb37b92ffd48
Nouman Tahir <nouman.hawkdev@gmail.com>
2022-12-21 19:57:31 +0500

    most free signup method to register modal

--- src/screens/register/registerContainer.js
+++ src/screens/register/registerContainer.js
@@ -1,19 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import { Alert } from 'react-native';
 import { useIntl } from 'react-intl';
 import { useSelector } from 'react-redux';
-import get from 'lodash/get';
 
-import { useNavigation } from '@react-navigation/native';
 import { lookupAccounts } from '../../providers/hive/dhive';
-import { signUp } from '../../providers/ecency/ecency';
-import ROUTES from '../../constants/routeNames';
 
 const RegisterContainer = ({ children }) => {
-  const navigation = useNavigation();
   const intl = useIntl();
 
-  const buyAccountModalRef = useRef(null);
+  const registerAccountModalRef = useRef(null);
 
   const isConnected = useSelector((state) => state.application.isConnected);
   const [isLoading, setIsLoading] = useState(false);
@@ -35,61 +30,12 @@ const RegisterContainer = ({ children }) => {
     }
   };
 
-  const _handleOnPressRegister = ({ username, email, refUsername }) => {
-    setIsLoading(true);
-
-    signUp(username, email, refUsername)
-      .then((result) => {
-        if (result) {
-          navigation.navigate(ROUTES.DRAWER.MAIN);
-          Alert.alert(
-            'Success',
-            'Hurrah, you did it! Expect email from us with further instructions.',
-          );
-        }
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        if (get(err, 'response.status') === 500) {
-          Alert.alert(
-            intl.formatMessage({ id: 'alert.fail' }),
-            intl.formatMessage({ id: 'register.500_error' }),
-          );
-        } else if (get(err, 'response.data.message')) {
-          Alert.alert(
-            intl.formatMessage({ id: 'alert.fail' }),
-            `${err.response.data.message}\nTry buying account instead`,
-            [
-              {
-                text: 'Buy Account',
-                onPress: () => {
-                  if (buyAccountModalRef.current) {
-                    buyAccountModalRef.current.showModal();
-                  }
-                },
-              },
-              {
-                text: 'Cancel',
-              },
-            ],
-          );
-        } else {
-          Alert.alert(
-            intl.formatMessage({ id: 'alert.fail' }),
-            intl.formatMessage({ id: 'alert.unknow_error' }),
-          );
-        }
-        setIsLoading(false);
-      });
-  };
-
   return (
     children &&
     children({
       getAccountsWithUsername: _getAccountsWithUsername,
-      handleOnPressRegister: _handleOnPressRegister,
       isLoading,
-      buyAccountModalRef,
+      registerAccountModalRef,
     })
   );
 };
