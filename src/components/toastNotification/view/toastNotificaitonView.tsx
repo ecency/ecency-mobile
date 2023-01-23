f9f57646ae7c3243d1f48a5b9abd0b3c0b262760
Sadaqat Ali <48380998+aliseyalvi@users.noreply.github.com>
2023-01-23 08:02:19 +0500

    * replaced animatable with rea2 in ToastNotification * change ToastNotification to functional component and tsx

---
+++ src/components/toastNotification/view/toastNotificaitonView.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import Animated, { SlideInDown, SlideOutDown } from 'react-native-reanimated';
+// import { View as AnimatedView } from 'react-native-animatable';
+
+// Styles
+import styles from './toastNotificationStyles';
+interface ToastNotificationProps {
+  duration: number;
+  text: string;
+  textStyle?: any;
+  style?: any;
+  onPress?: () => void;
+  onHide: () => void;
+}
+const ToastNotification = ({
+  duration,
+  text,
+  textStyle,
+  style,
+  onPress,
+  onHide,
+}: ToastNotificationProps) => {
+  // Component Life Cycles
+  useEffect(() => {
+    if (duration) {
+      const closeTimer = setTimeout(() => {
+        _hideToast();
+      }, duration);
+      return () => {
+        clearTimeout(closeTimer);
+      };
+    }
+  }, []);
+
+  // Component Functions
+
+  const _hideToast = () => {
+    if (onHide) {
+      onHide();
+    }
+  };
+
+  // const finalPosition = isTop ? { top: 100 } : { bottom: 100 };
+  return (
+    <TouchableOpacity disabled={!onPress} onPress={() => onPress && onPress()}>
+      <Animated.View
+        style={{
+          ...styles.container,
+          ...style,
+        }}
+        entering={SlideInDown.duration(750)}
+        exiting={SlideOutDown.duration(500)}
+      >
+        <Text style={[styles.text, textStyle]}>{text}</Text>
+      </Animated.View>
+    </TouchableOpacity>
+  );
+};
+
+export default ToastNotification;
