bbc4e0c5af59e6e28ac20318ee6b0a137896391e
Nouman Tahir <nouman.hawkdev@gmail.com>
2023-03-07 18:17:25 +0500

    calculate initial height based on provided image_ratios

--- src/components/autoHeightImage/autoHeightImage.tsx
+++ src/components/autoHeightImage/autoHeightImage.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { proxifyImageSrc } from '@ecency/render-helper';
+import React, { useEffect, useMemo, useState } from 'react';
+import { Image, Platform, TouchableOpacity } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import FastImage from 'react-native-fast-image';
 
@@ -20,31 +21,54 @@ export const AutoHeightImage = ({
   activeOpacity,
   onPress,
 }: AutoHeightImageProps) => {
+
+  //extract iniital height based on provided ratio
+  const _initialHeight = useMemo(() => {
+    let _height = contentWidth / (16 / 9)
+    if (metadata && metadata.image && metadata.image_ratios) {
+      metadata.image_ratios.forEach((_ratio, index) => {
+        const url = metadata.image[index];
+
+        if(url && !isNaN(_ratio)){
+          const poxifiedUrl = proxifyImageSrc(url, undefined, undefined, Platform.select({
+            ios: 'match', android: 'webp'
+          }));
+
+          if (imgUrl === poxifiedUrl) {
+            const _ratio = metadata.image_ratios[index];
+            _height = contentWidth / _ratio;
+          }
+        }
+
+      })
+    }
+    return _height;
+  }, [imgUrl])
+
+
   const [imgWidth, setImgWidth] = useState(contentWidth);
-  const [imgHeight, setImgHeight] = useState(imgWidth * (9 / 16));
+  const [imgHeight, setImgHeight] = useState(_initialHeight);
   const [onLoadCalled, setOnLoadCalled] = useState(false);
 
-  useEffect(() => {
-    _fetchImageBounds();
-  }, []);
 
-  const _fetchImageBounds = () => {
-    Image.getSize(imgUrl, (width, height) => {
-      const newWidth = width < contentWidth ? width : contentWidth;
-      const newHeight = (height / width) * newWidth;
-      setImgHeight(newHeight);
-      setImgWidth(newWidth);
-    });
-  };
+  //NOTE: important to have post image bound set even for images with ratio already provided
+  // as this handles the case where width can be lower than contentWidth
+  const _setImageBounds = (width, height) => {
+    const newWidth = width < contentWidth ? width : contentWidth;
+    const newHeight = (height / width) * newWidth;
+    setImgHeight(newHeight);
+    setImgWidth(newWidth);
+  }
 
   const imgStyle = {
-    width: imgWidth - 10,
+    width: imgWidth,
     height: imgHeight,
     backgroundColor: onLoadCalled ? 'transparent' : EStyleSheet.value('$primaryLightBackground'),
   };
 
-  const _onLoad = () => {
+  const _onLoad = (evt) => {
     setOnLoadCalled(true);
+    _setImageBounds(evt.nativeEvent.width, evt.nativeEvent.height);
   };
 
   return (
