a0c6f67f7155eccfcb211523fd52881dfb17852d
Sadaqat Ali <48380998+aliseyalvi@users.noreply.github.com>
2023-05-04 23:55:45 +0500

    added sign keys method to import keys modal

--- src/screens/backupKeysScreen/importPrivateKeyModal.tsx
+++ src/screens/backupKeysScreen/importPrivateKeyModal.tsx
@@ -1,23 +1,74 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import React, { forwardRef, useImperativeHandle, useState } from 'react';
 import { useIntl } from 'react-intl';
 import { Modal, TextInput } from '../../components';
+import { useAppSelector } from '../../hooks';
+import { useDispatch } from 'react-redux';
 
 // styles
 import styles from './backupKeysScreenStyles';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
+// redux / providers
+import { getUpdatedUserKeys } from '../../providers/hive/auth';
+import { getDigitPinCode } from '../../providers/hive/dhive';
+import { updateCurrentAccount } from '../../redux/actions/accountAction';
+
+// import reactotron from 'reactotron-react-native';
+
 type Props = {};
 
 export const ImportPrivateKeyModalModal = forwardRef(({}: Props, ref) => {
   const intl = useIntl();
+  const dispatch = useDispatch();
+
+  const currentAccount = useAppSelector((state) => state.account.currentAccount);
+  const pinCode = useAppSelector((state) => state.application.pin);
+  const digitPinCode = getDigitPinCode(pinCode);
+
   const [showModal, setShowModal] = useState(false);
+  const [key, setKey] = useState('');
 
   useImperativeHandle(ref, () => ({
     showModal: () => {
       setShowModal(true);
     },
   }));
+
+  //  sign key with password or master key
+  const _handleSignKey = async () => {
+    const data = {
+      username: currentAccount.username,
+      password: key,
+      pinCode: digitPinCode,
+    };
+
+    getUpdatedUserKeys(currentAccount, data)
+      .then((result) => {
+        if (result) {
+          //   reactotron.log('result : ', result);
+          // Save user data to Realm DB
+          // await setUserData(updatedUserData);
+          // update user data in redux
+          dispatch(updateCurrentAccount({ ...result }));
+          setShowModal(false);
+        }
+      })
+      .catch((err) => {
+        // if error description exist, pass it to alert else pass error message key
+        const errorDescription = err?.response?.data?.error_description
+          ? err?.response?.data?.error_description
+          : intl.formatMessage({
+              id: err.message,
+            });
+        Alert.alert(
+          intl.formatMessage({
+            id: 'settings.backup_keys_modal.import_failed',
+          }),
+          ` ${errorDescription}\n${intl.formatMessage({ id: 'login.login_failed_body' })}`, // append login body failure key
+        );
+      });
+  };
 
   const _renderContent = () => (
     <View style={styles.modalContent}>
@@ -33,8 +84,9 @@ export const ImportPrivateKeyModalModal = forwardRef(({}: Props, ref) => {
           })}
           placeholderTextColor={EStyleSheet.value('$borderColor')}
           style={styles.signInput}
+          onChangeText={setKey}
         />
-        <TouchableOpacity style={styles.signBtn}>
+        <TouchableOpacity style={styles.signBtn} onPress={_handleSignKey}>
           <Text style={styles.signBtnText}>
             {intl.formatMessage({ id: 'settings.backup_keys_modal.sign' })}
           </Text>
