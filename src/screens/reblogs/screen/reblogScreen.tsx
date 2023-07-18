0ebe982b883b0c042d333686cb0d8b17bf7139af
Syed Moazzam Ali <moazzambukhari52@gmail.com>
2023-07-18 18:38:33 +0500

    add reblog post button in reblogScreen

--- src/screens/reblogs/screen/reblogScreen.tsx
+++ src/screens/reblogs/screen/reblogScreen.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { FlatList, SafeAreaView } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView, RefreshControl } from 'react-native';
 import { useIntl } from 'react-intl';
 import get from 'lodash/get';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import reactotron from 'reactotron-react-native';
 import { useAppDispatch, useAppSelector } from '../../../hooks';
 import showLoginAlert from '../../../utils/showLoginAlert';
 import { reblog } from '../../../providers/hive/dhive';
 import { useUserActivityMutation } from '../../../providers/queries/pointQueries';
+import { getPostReblogs } from '../../../providers/ecency/ecency';
 // Components
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { BasicHeader, MainButton, UserListItem } from '../../../components';
 
 // Container
@@ -16,31 +18,43 @@ import AccountListContainer from '../../../containers/accountListContainer';
 // Utils
 import globalStyles from '../../../globalStyles';
 import { getTimeFromNow } from '../../../utils/time';
-const renderUserListItem = (item, index, handleOnUserPress, isLoading) => {
+import { setRcOffer, toastNotification } from '../../../redux/actions/uiAction';
+import { PointActivityIds } from '../../../providers/ecency/ecency.types';
+
+const renderUserListItem = (item, index, handleOnUserPress) => {
   return (
     <UserListItem
       index={index}
       username={item.account}
       description={getTimeFromNow(item.timestamp)}
       handleOnPress={() => handleOnUserPress(item.account)}
-      isClickable
-    />
+      // eslint-disable-next-line max-len
+      isClickable rightText={undefined} descriptionStyle={undefined} subRightText={undefined} isRightColor={undefined} isHasRightItem={undefined} isBlackRightColor={undefined} itemIndex={undefined} handleOnLongPress={undefined} text={undefined} middleText={undefined} rightTextStyle={undefined} onPressRightText={undefined} isLoggedIn={undefined} searchValue={undefined} rightTooltipText={undefined} leftItemRenderer={undefined} rightItemRenderer={undefined} />
   );
 };
 
 const ReblogScreen = ({ route }) => {
+  const pReblogs = route.params?.reblogs;
+  const [reblogs, setReblogs] = useState(pReblogs);
+
+  getPostReblogs(reblogs).then((result) => {
+    reactotron.log(result);
+  });
+  const [refreshing, setRefreshing] = React.useState(false);
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+
+  const content = route.params?.content;
   const intl = useIntl();
   const headerTitle = intl.formatMessage({
     id: 'reblog.title',
   });
-  // const _reblog = () => {
-  //   alert("hello");
-  // }
-
-
-
   const dispatch = useAppDispatch();
-  // const navigation = useNavigation();
   const userActivityMutation = useUserActivityMutation();
   const isLoggedIn = useAppSelector((state) => state.application.isLoggedIn);
   const currentAccount = useAppSelector((state) => state.account.currentAccount);
@@ -51,13 +65,15 @@ const ReblogScreen = ({ route }) => {
       showLoginAlert({ intl });
       return;
     }
+
     if (isLoggedIn) {
-      reblog(currentAccount, pinCode, content.author, get(content, 'permlink', ''))
+      reblog(currentAccount, pinCode, content.author, content.permlink)
         .then((response) => {
           // track user activity points ty=130
           userActivityMutation.mutate({
             pointsTy: PointActivityIds.REBLOG,
             transactionId: response.id,
+
           });
 
           dispatch(
@@ -90,27 +106,28 @@ const ReblogScreen = ({ route }) => {
     }
   };
 
-
-
-
-
-  const reblogs = route.params?.reblogs;
-
   return (
     <AccountListContainer data={reblogs}>
       {({ data, filterResult, handleSearch, handleOnUserPress }) => (
-        <SafeAreaView style={[globalStyles.container, { paddingBottom: 40 }]}>
+        <SafeAreaView style={[globalStyles.container, { paddingBottom: 40 }]}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
           <BasicHeader
             title={`${headerTitle} (${data && data.length})`}
             backIconName="close"
             isHasSearch
             handleOnSearch={(text) => handleSearch(text, 'account')}
           />
+
           <FlatList
             data={filterResult || data}
             keyExtractor={(item) => item.account}
             removeClippedSubviews={false}
-            renderItem={({ item, index }) => renderUserListItem(item, index, handleOnUserPress)}
+            // eslint-disable-next-line max-len, no-undef
+            renderItem={({ item, index }) =>
+              renderUserListItem(item, index, handleOnUserPress)}
           />
           <MainButton
             style={globalStyles.mainbutton}
