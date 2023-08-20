c026970dba750ffcb7317e36ae9adbd57a76bdeb
Nouman Tahir <noumantahir@live.com>
2023-08-20 23:17:03 +0500

    rendering first set of ecency waves

--- src/screens/waves/screen/wavesScreen.tsx
+++ src/screens/waves/screen/wavesScreen.tsx
@@ -1,60 +1,37 @@
-import React from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
-import { Header } from '../../../components';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import { Comments, Header } from '../../../components';
+import { getComments } from '../../../providers/hive/dhive';
+import styles from '../styles/wavesScreen.styles';
 
-interface Item {
-  id: number;
-  title: string;
-}
 
-const DATA: Item[] = [
-  { id: 1, title: 'Item 1' },
-  { id: 2, title: 'Item 2' },
-  { id: 3, title: 'Item 3' },
-];
+const WavesScreen = () => {
 
-const WavesScreen: React.FC = () => {
-  const renderItem = ({ item }: { item: Item }) => (
-    <View style={styles.item}>
-      <Text>{item.title}</Text>
-    </View>
-  );
 
-  return (
-    <View style={styles.container}>
-        <Header />
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Header Title</Text>
-      </View>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-      />
-    </View>
-  );
+    const [waves, setWaves] = useState([]);
+
+    useEffect(() => {
+        _fetchWaves()
+    }, [])
+
+    const _fetchWaves = async () => {
+        const data = await getComments('ecency.waves', 'waves-2023-08-19');
+        setWaves(data)
+    }
+
+    return (
+        <View style={styles.container}>
+            <Header />
+
+            <View style={{ flex: 1 }}>
+                <Comments
+                    comments={waves}
+                />
+            </View>
+
+        </View>
+    );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  header: {
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-    alignItems: 'center',
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  item: {
-    padding: 20,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ccc',
-  },
-});
 
 export default WavesScreen;
