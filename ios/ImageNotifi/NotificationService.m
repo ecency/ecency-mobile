0b4e09920122ad06298c7795a9273d382a905682
feruz <fmiboy@gmail.com>
2023-10-12 09:22:59 +0300

    change target of notification extention ios

--- ios/ImageNotifi/NotificationService.m
+++ ios/ImageNotifi/NotificationService.m
@@ -22,8 +22,6 @@
     self.bestAttemptContent = [request.content mutableCopy];
     
     // Modify the notification content here...
-    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]",   self.bestAttemptContent.title];
-
     [[FIRMessaging extensionHelper] populateNotificationContent:self.bestAttemptContent withContentHandler:contentHandler];
 }
 
