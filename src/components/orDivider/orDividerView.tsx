9f53fa3c01d2e207d81b398c3fc2642a3044aba9
Sadaqat Ali <48380998+aliseyalvi@users.noreply.github.com>
2023-12-11 15:25:01 +0500

    * used intl for OR deivider

--- src/components/orDivider/orDividerView.tsx
+++ src/components/orDivider/orDividerView.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { View, Text, ViewStyle } from 'react-native';
+import { useIntl } from 'react-intl';
 import styles from './orDividerStyles';
 
 interface OrDividerProps {
   containerStyle?: ViewStyle;
 }
 const OrDivider = ({ containerStyle }: OrDividerProps) => {
+  const intl = useIntl();
   return (
     <View style={[styles.dividerContainer, containerStyle]}>
       <View style={[styles.divider, styles.leftDivider]} />
-      <Text style={styles.orText}>OR</Text>
+      <Text style={styles.orText}>
+        {intl.formatMessage({
+          id: 'login.or',
+        })}
+      </Text>
       <View style={[styles.divider, styles.rightDivider]} />
     </View>
   );
