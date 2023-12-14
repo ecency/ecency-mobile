d9cc3652cc1055b77001f1774d1e38ebae553af0
noumantahir <nouman.hawkdev@gmail.com>
2023-12-14 13:38:19 +0500

    added custom reply modal backdrop

--- src/components/quickReplyModal/quickReplyModalView.tsx
+++ src/components/quickReplyModal/quickReplyModalView.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ActionSheet from 'react-native-actions-sheet';
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
 import { QuickReplyModalContent } from './quickReplyModalContent';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { hideReplyModal } from '../../redux/actions/uiAction';
@@ -10,17 +11,21 @@ const QuickReplyModal = () => {
   const dispatch = useAppDispatch();
   const bottomSheetModalRef = useRef();
 
+  const [backdropVisible, setBackdropVisible] = useState(false);
+
   const replyModalVisible = useAppSelector((state) => state.ui.replyModalVisible);
   const replyModalData: PostEditorModalData = useAppSelector((state) => state.ui.replyModalData);
   const modalContentRef = useRef(null);
 
   useEffect(() => {
     if (replyModalVisible) {
+      setBackdropVisible(true);
       bottomSheetModalRef.current?.show();
     }
   }, [replyModalVisible]);
 
   const _onClose = () => {
+    setBackdropVisible(false);
     bottomSheetModalRef.current?.hide();
     if (modalContentRef.current) {
       modalContentRef.current.handleSheetClose();
@@ -29,22 +34,26 @@ const QuickReplyModal = () => {
   };
 
   return (
-    <ActionSheet
-      ref={bottomSheetModalRef}
-      gestureEnabled={true}
-      containerStyle={styles.sheetContent}
-      indicatorStyle={styles.sheetIndicator}
-      defaultOverlayOpacity={0}
-      elevation={50}
-      onClose={_onClose}
-    >
-      <QuickReplyModalContent
-        ref={modalContentRef}
-        mode={replyModalData?.mode || 'comment'}
-        selectedPost={replyModalData?.parentPost}
+    <>
+      <ActionSheet
+        ref={bottomSheetModalRef}
+        gestureEnabled={true}
+        containerStyle={styles.sheetContent}
+        indicatorStyle={styles.sheetIndicator}
+        defaultOverlayOpacity={0}
         onClose={_onClose}
-      />
-    </ActionSheet>
+      >
+        <QuickReplyModalContent
+          ref={modalContentRef}
+          mode={replyModalData?.mode || 'comment'}
+          selectedPost={replyModalData?.parentPost}
+          onClose={_onClose}
+        />
+      </ActionSheet>
+      {backdropVisible && (
+        <Animated.View exiting={FadeOut} entering={FadeIn} style={styles.backdrop} />
+      )}
+    </>
   );
 };
 
