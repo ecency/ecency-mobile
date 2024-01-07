76c88bf3ba19bf8ed4c0c9db28e49a8137010899
Sadaqat Ali <48380998+aliseyalvi@users.noreply.github.com>
2024-01-07 20:42:45 +0500

    * added custom lodash to fix characters missing issue * added memo in Tab View component to avoid unnecessary re renders

--- src/screens/searchResult/screen/searchResultScreen.tsx
+++ src/screens/searchResult/screen/searchResultScreen.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { memo, useState } from 'react';
 import { View } from 'react-native';
 import ScrollableTabView from 'react-native-scrollable-tab-view';
 import { useIntl } from 'react-intl';
-import { debounce } from 'lodash';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import useDebounce from '../../../utils/useDebounceHook';
 
 // Components
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { SearchInput, TabBar } from '../../../components';
 import Communities from './tabs/communities/view/communitiesResults';
 import PostsResults from './tabs/best/view/postsResults';
@@ -17,12 +17,50 @@ import styles from './searchResultStyles';
 import globalStyles from '../../../globalStyles';
 
 const SearchResultScreen = ({ navigation }) => {
-  const [searchValue, setSearchValue] = useState('');
   const intl = useIntl();
+  const { debounce } = useDebounce();
+
+  const [searchInputValue, setSearchInputValue] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+
+  const _handleChangeText = (value) => {
+    setSearchInputValue(value);
+  };
+
+  const _handleSearchValue = (value) => {
+    setSearchValue(value);
+  };
+
+  // custom debounce to debounce search value but updates search input value instantly
+  // fixes character missing bug due to lodash debounce
+  const debouncedSearch = debounce(_handleSearchValue, _handleChangeText, 1000);
 
   const _navigationGoBack = () => {
     navigation.goBack();
   };
+
+  return (
+    <View style={styles.container}>
+      <SearchInput
+        showClearButton={true}
+        placeholder={intl.formatMessage({ id: 'header.search' })}
+        onChangeText={debouncedSearch}
+        value={searchInputValue}
+        backEnabled={true}
+        onBackPress={_navigationGoBack}
+      />
+      <SearchResultsTabView searchValue={searchValue} />
+    </View>
+  );
+};
+
+const SearchResultsTabView = memo(({ searchValue }: { searchValue: string }) => {
+  const intl = useIntl();
+
+  const clippedSearchValue =
+    searchValue.startsWith('#') || searchValue.startsWith('@')
+      ? searchValue.substring(1)
+      : searchValue;
 
   const _renderTabbar = () => (
     <TabBar
@@ -34,58 +72,38 @@ const SearchResultScreen = ({ navigation }) => {
     />
   );
 
-  const _handleChangeText = debounce((value) => {
-    setSearchValue(value);
-  }, 1000);
-
-  const clippedSearchValue =
-    searchValue.startsWith('#') || searchValue.startsWith('@')
-      ? searchValue.substring(1)
-      : searchValue;
-
   return (
-    <View style={styles.container}>
-      <SearchInput
-        showClearButton={true}
-        placeholder={intl.formatMessage({ id: 'header.search' })}
-        onChangeText={_handleChangeText}
-        value={searchValue}
-        backEnabled={true}
-        onBackPress={_navigationGoBack}
-      />
-
-      <ScrollableTabView
-        style={globalStyles.tabView}
-        renderTabBar={_renderTabbar}
-        prerenderingSiblingsNumber={Infinity}
+    <ScrollableTabView
+      style={globalStyles.tabView}
+      renderTabBar={_renderTabbar}
+      prerenderingSiblingsNumber={Infinity}
+    >
+      <View
+        tabLabel={intl.formatMessage({ id: 'search_result.best.title' })}
+        style={styles.tabbarItem}
       >
-        <View
-          tabLabel={intl.formatMessage({ id: 'search_result.best.title' })}
-          style={styles.tabbarItem}
-        >
-          <PostsResults searchValue={clippedSearchValue} />
-        </View>
-        <View
-          tabLabel={intl.formatMessage({ id: 'search_result.people.title' })}
-          style={styles.tabbarItem}
-        >
-          <PeopleResults searchValue={clippedSearchValue} />
-        </View>
-        <View
-          tabLabel={intl.formatMessage({ id: 'search_result.topics.title' })}
-          style={styles.tabbarItem}
-        >
-          <TopicsResults searchValue={clippedSearchValue} />
-        </View>
-        <View
-          tabLabel={intl.formatMessage({ id: 'search_result.communities.title' })}
-          style={styles.tabbarItem}
-        >
-          <Communities searchValue={clippedSearchValue} />
-        </View>
-      </ScrollableTabView>
-    </View>
+        <PostsResults searchValue={clippedSearchValue} />
+      </View>
+      <View
+        tabLabel={intl.formatMessage({ id: 'search_result.people.title' })}
+        style={styles.tabbarItem}
+      >
+        <PeopleResults searchValue={searchValue} />
+      </View>
+      <View
+        tabLabel={intl.formatMessage({ id: 'search_result.topics.title' })}
+        style={styles.tabbarItem}
+      >
+        <TopicsResults searchValue={clippedSearchValue} />
+      </View>
+      <View
+        tabLabel={intl.formatMessage({ id: 'search_result.communities.title' })}
+        style={styles.tabbarItem}
+      >
+        <Communities searchValue={clippedSearchValue} />
+      </View>
+    </ScrollableTabView>
   );
-};
+});
 
 export default gestureHandlerRootHOC(SearchResultScreen);
