b969f7aa36347db90fdc5508bf42ff5a1782e725
Nouman Tahir <noumantahir@live.com>
2024-02-04 22:28:05 +0500

    handling hiveuri directly from comments and post body

--- src/components/qrModal/qrModalView.tsx
+++ src/components/qrModal/qrModalView.tsx
@@ -9,6 +9,7 @@ import * as hiveuri from 'hive-uri';
 import styles from './qrModalStyles';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import {
+  handleHiveUri,
   showActionModal,
   showWebViewModal,
   toastNotification,
@@ -36,20 +37,18 @@ export const QRModal = () => {
   const isPinCodeOpen = useAppSelector((state) => state.application.isPinCodeOpen);
   const isLoggedIn = useAppSelector((state) => state.application.isLoggedIn);
 
-
   const [isScannerActive, setIsScannerActive] = useState(true);
   const [isProcessing, setIsProcessing] = useState(false);
   const sheetModalRef = useRef<ActionSheet>();
   const scannerRef = useRef(null);
 
-  //TODO: make sure to properly clean uri processing code to process uri from deep links and notifications
+  // TODO: make sure to properly clean uri processing code to process uri from deep links and notifications
   const hiveUriToHandle = useAppSelector((state) => state.ui.hiveUriToHandle);
   useEffect(() => {
-    if(hiveUriToHandle){
+    if (hiveUriToHandle) {
       _handleHiveUri(hiveUriToHandle);
     }
-  }, [hiveUriToHandle])
-
+  }, [hiveUriToHandle]);
 
   useEffect(() => {
     if (isVisibleQRModal) {
@@ -58,10 +57,7 @@ export const QRModal = () => {
     } else {
       sheetModalRef?.current?.hide();
     }
-
   }, [isVisibleQRModal]);
-
-
 
   const requestCameraPermission = async () => {
     if (Platform.OS === 'ios') {
@@ -218,6 +214,7 @@ export const QRModal = () => {
                 },
               },
             ],
+            onClosed: () => dispatch(handleHiveUri('')),
           }),
         );
       })
