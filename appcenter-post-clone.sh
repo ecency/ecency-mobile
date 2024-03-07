d4ad1714fab579d59c911229f97f9d9057ab6bf4
noumantahir <nouman.hawkdev@gmail.com>
2024-03-07 18:33:19 +0500

    script to avoid running pod install on android platform

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -16,3 +16,18 @@ node -v
 #node -v
 
 printf "end of post-clone.sh\n"
+
+echo "Selected Platform to buit"
+echo "$PLATFORM"
+
+#check if platform to be built for is android
+if [ "$PLATFORM" == "Android" ]; then
+    # Read package.json into a variable
+    package_json=$(<package.json)
+
+    # Remove 'pod install' from the current postinstall script
+    updated_package_json=$(echo "$package_json" | sed 's/ && cd ios && pod install//g')
+
+    # Update package.json with the modified postinstall script
+    echo "$updated_package_json" > package.json
+fi
