44eeecdb221b340e371f5b0883d0819062852335
noumantahir <nouman.hawkdev@gmail.com>
2024-04-23 19:30:36 +0500

    cleaned up post interaction handler

--- src/components/postHtmlRenderer/postInteractionHandler.tsx
+++ src/components/postHtmlRenderer/postInteractionHandler.tsx
@@ -1,13 +1,9 @@
 import React, { forwardRef, useImperativeHandle, useRef, useState, Fragment } from 'react';
-import { PermissionsAndroid, Platform, SafeAreaView, View, Text } from 'react-native';
 import { useIntl } from 'react-intl';
 import ActionsSheet from 'react-native-actions-sheet';
-import ImageView from 'react-native-image-viewing';
 
 // Components
 import EStyleSheet from 'react-native-extended-stylesheet';
-// import {CameraRoll} from '@react-native-camera-roll/camera-roll';
-import RNFetchBlob from 'rn-fetch-blob';
 import { useDispatch } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import ROUTES from '../../constants/routeNames';
@@ -17,8 +13,6 @@ import { writeToClipboard } from '../../utils/clipboard';
 import { OptionsModal } from '../atoms';
 import VideoPlayer from '../videoPlayer/videoPlayerView';
 
-import { IconButton } from '../buttons';
-import styles from './postHtmlRendererStyles';
 import { PostTypes } from '../../constants/postTypes';
 import { isHiveUri } from '../../utils/hive-uri';
 import { ImageViewer } from '../imageViewer';
@@ -29,28 +23,23 @@ interface PostHtmlInteractionHandlerProps {
 
 export const PostHtmlInteractionHandler = forwardRef(
   ({ postType }: PostHtmlInteractionHandlerProps, ref) => {
+    console.log('Post Type', postType)
+
     const navigation = useNavigation();
     const dispatch = useDispatch();
     const intl = useIntl();
 
-    const actionImage = useRef(null);
     const actionLink = useRef(null);
     const youtubePlayerRef = useRef(null);
     const imageViewerRef = useRef(null);
 
-    const [postImages, setPostImages] = useState<string[]>([]);
-    const [isImageModalOpen, setIsImageModalOpen] = useState(false);
-
     const [videoUrl, setVideoUrl] = useState(null);
     const [youtubeVideoId, setYoutubeVideoId] = useState(null);
     const [videoStartTime, setVideoStartTime] = useState(0);
-    const [selectedImage, setSelectedImage] = useState(null);
     const [selectedLink, setSelectedLink] = useState(null);
 
     useImperativeHandle(ref, () => ({
       handleImagePress: (url: string, postImgUrls: string[]) => {
-        setPostImages(postImgUrls);
-        setSelectedImage(url);
         if(imageViewerRef.current){
           imageViewerRef.current.show(url, postImgUrls);
         }
@@ -80,97 +69,8 @@ export const PostHtmlInteractionHandler = forwardRef(
       },
     }));
 
-    const checkAndroidPermission = async () => {
-      try {
-        const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-        await PermissionsAndroid.request(permission);
-        Promise.resolve();
-      } catch (error) {
-        Promise.reject(error);
-      }
-    };
 
-    const _downloadImage = async (uri) => {
-      return RNFetchBlob.config({
-        fileCache: true,
-        appendExt: 'jpg',
-      })
-        .fetch('GET', uri)
-        .then((res) => {
-          const { status } = res.info();
 
-          if (status == 200) {
-            return res.path();
-          } else {
-            Promise.reject();
-          }
-        })
-        .catch((errorMessage) => {
-          Promise.reject(errorMessage);
-        });
-    };
-
-    const _saveImage = async (uri) => {
-      try {
-        if (Platform.OS === 'android') {
-          await checkAndroidPermission();
-          uri = `file://${await _downloadImage(uri)}`;
-        }
-        // CameraRoll.saveToCameraRoll(uri)
-        //   .then(() => {
-        //     dispatch(
-        //       toastNotification(
-        //         intl.formatMessage({
-        //           id: 'post.image_saved',
-        //         }),
-        //       ),
-        //     );
-        //   })
-        //   .catch(() => {
-        //     dispatch(
-        //       toastNotification(
-        //         intl.formatMessage({
-        //           id: 'post.image_saved_error',
-        //         }),
-        //       ),
-        //     );
-        //   });
-      } catch (error) {
-        dispatch(
-          toastNotification(
-            intl.formatMessage({
-              id: 'post.image_saved_error',
-            }),
-          ),
-        );
-      }
-    };
-
-    const _handleImageOptionPress = (ind) => {
-      if (ind === 1) {
-        // open gallery mode
-        setIsImageModalOpen(true);
-        return;
-      }
-      if (ind === 0) {
-        // copy to clipboard
-        writeToClipboard(selectedImage).then(() => {
-          dispatch(
-            toastNotification(
-              intl.formatMessage({
-                id: 'alert.copied',
-              }),
-            ),
-          );
-        });
-      }
-      // if (ind === 2) {
-      //   // save to local
-      //   _saveImage(selectedImage);
-      // }
-
-      setSelectedImage(null);
-    };
 
     const _handleLinkOptionPress = (ind) => {
       if (ind === 1) {
@@ -201,64 +101,11 @@ export const PostHtmlInteractionHandler = forwardRef(
       setSelectedLink(null);
     };
 
-    const _onCloseImageViewer = () => {
-      setIsImageModalOpen(false);
-      setSelectedImage(null);
-    };
-
-    const _renderImageViewerHeader = (imageIndex) => {
-      return (
-        <SafeAreaView
-          style={{
-            marginTop: Platform.select({ ios: 0, android: 25 }),
-          }}
-        >
-          <View style={styles.imageViewerHeaderContainer}>
-            <Text style={styles.imageGalleryHeaderText}>{`${imageIndex + 1}/${
-              postImages.length
-            }`}</Text>
-            <IconButton
-              name="close"
-              color={EStyleSheet.value('$primaryDarkText')}
-              buttonStyle={styles.closeIconButton}
-              size={20}
-              handleOnPress={_onCloseImageViewer}
-            />
-          </View>
-        </SafeAreaView>
-      );
-    };
-
     return (
       <Fragment>
 
         <ImageViewer 
           ref={imageViewerRef}
-        />
-        {/* <ImageView
-          images={postImages.map((url) => ({ uri: url }))}
-          imageIndex={postImages.indexOf(selectedImage)}
-          visible={isImageModalOpen}
-          animationType="slide"
-          swipeToCloseEnabled
-          onRequestClose={_onCloseImageViewer}
-          HeaderComponent={(imageIndex) => _renderImageViewerHeader(imageIndex.imageIndex)}
-        /> */}
-
-        {/* Get rid of actions modal here */}
-        <OptionsModal
-          ref={actionImage}
-          options={[
-            intl.formatMessage({ id: 'post.copy_link' }),
-            intl.formatMessage({ id: 'post.gallery_mode' }),
-            // intl.formatMessage({ id: 'post.save_to_local' }),
-            intl.formatMessage({ id: 'alert.cancel' }),
-          ]}
-          title={intl.formatMessage({ id: 'post.image' })}
-          cancelButtonIndex={2}
-          onPress={(index) => {
-            _handleImageOptionPress(index);
-          }}
         />
 
         <OptionsModal
