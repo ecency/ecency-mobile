cd27d2cdd93cac555caf86fe63186820f3571ea0
noumantahir <nouman.hawkdev@gmail.com>
2024-06-06 18:25:32 +0500

    poll wizard default to default draft id for fetching local poll data

--- src/screens/pollWizard/screen/pollWizardScreen.tsx
+++ src/screens/pollWizard/screen/pollWizardScreen.tsx
@@ -4,12 +4,13 @@ import { View } from "react-native";
 import styles from "../styles/pollWizardScreen.styles";
 import { BasicHeader } from "../../../components";
 import { useIntl } from "react-intl";
+import { DEFAULT_USER_DRAFT_ID } from "../../../redux/constants/constants";
 
 
 const PollWizardScreen = ({ route }) => {
 
     const intl = useIntl();
-    const draftId = route.params?.draftId;
+    const draftId = route.params?.draftId || DEFAULT_USER_DRAFT_ID;
 
     return (
         <View style={styles.container}>
