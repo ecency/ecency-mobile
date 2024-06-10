8f51a8eb65abb12a85a6f5d6cbac8c72e78963e3
noumantahir <nouman.hawkdev@gmail.com>
2024-06-10 16:57:38 +0500

    using modal header instead of basic header in poll wizard screen

--- src/screens/pollWizard/screen/pollWizardScreen.tsx
+++ src/screens/pollWizard/screen/pollWizardScreen.tsx
@@ -2,21 +2,26 @@ import React from "react"
 import { PollsWizardContent } from "../../../components/pollsWizardModal/children/pollsWizardContent"
 import { View } from "react-native";
 import styles from "../styles/pollWizardScreen.styles";
-import { BasicHeader } from "../../../components";
+import { ModalHeader } from "../../../components";
 import { useIntl } from "react-intl";
 import { DEFAULT_USER_DRAFT_ID } from "../../../redux/constants/constants";
+import { useNavigation } from "@react-navigation/native";
 
 
 const PollWizardScreen = ({ route }) => {
 
     const intl = useIntl();
+    const navigation = useNavigation();
     const draftId = route.params?.draftId || DEFAULT_USER_DRAFT_ID;
 
     return (
         <View style={styles.container}>
-            <BasicHeader
+            <ModalHeader
                 title={intl.formatMessage({ id: 'post_poll.create_title' })}
-                backIconName="close"
+                isCloseButton={true}
+                onClosePress={()=>{
+                    navigation.goBack();
+                }}
             />
             <PollsWizardContent draftId={draftId} />
         </View>
