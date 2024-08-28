bf4934f883b72f0edc21e2ac54595523b46ce5df
noumantahir <nouman.hawkdev@gmail.com>
2024-08-28 18:14:42 +0500

    tab content clean up

--- src/components/tabbedPosts/view/tabContent.tsx
+++ src/components/tabbedPosts/view/tabContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   NativeScrollEvent,
@@ -7,21 +7,13 @@ import {
 import { debounce } from 'lodash';
 import BackgroundTimer from 'react-native-background-timer';
 import PostsList from '../../postsList';
-import { fetchPromotedEntries, loadPosts } from '../services/tabbedPostsFetch';
-import { LoadPostsOptions, TabContentProps, TabMeta } from '../services/tabbedPostsModels';
+import { TabContentProps } from '../services/tabbedPostsModels';
 import TabEmptyView from './listEmptyView';
 import { showReplyModal } from '../../../redux/actions/uiAction';
 import { PostsListRef } from '../../postsList/container/postsListContainer';
 import ScrollTopPopup from './scrollTopPopup';
-import { useFeedQuery } from '../../../providers/queries/postQueries/feedQueries';
 import { useFeedQuery, usePromotedPostsQuery } from '../../../providers/queries/postQueries/feedQueries';
 
-const DEFAULT_TAB_META = {
-  startAuthor: '',
-  startPermlink: '',
-  isLoading: false,
-  isRefreshing: false,
-} as TabMeta;
 
 let scrollOffset = 0;
 let blockPopup = false;
@@ -40,17 +32,13 @@ const TabContent = ({
   onScrollRequestProcessed,
   handleOnScroll,
   handleOnScrollBeginDrag,
-  ...props
 }: TabContentProps) => {
-  let _isMounted = true;
 
   // redux properties
   const dispatch = useDispatch();
   const isLoggedIn = useSelector((state) => state.application.isLoggedIn);
-  const nsfw = useSelector((state) => state.application.nsfw);
   const isConnected = useSelector((state) => state.application.isConnected);
   const currentAccount = useSelector((state) => state.account.currentAccount);
-  const initPosts = useSelector((state) => state.posts.initPosts);
 
   const { username } = currentAccount;
   const userPinned = currentAccount.about?.profile?.pinned;
@@ -65,21 +53,17 @@ const TabContent = ({
   const promotedPostsQuery = usePromotedPostsQuery();
 
   // state
-  const [posts, setPosts] = useState([]);
   const [sessionUser, setSessionUser] = useState(username);
-  const [tabMeta, setTabMeta] = useState(DEFAULT_TAB_META);
   const [enableScrollTop, setEnableScrollTop] = useState(false);
   const [curPinned, setCurPinned] = useState(pinnedPermlink);
 
   // refs
   const postsListRef = useRef<PostsListRef>();
 
-  const postsRef = useRef(posts);
   const sessionUserRef = useRef(sessionUser);
   const postFetchTimerRef = useRef<any>(null);
 
   // init state refs;
-  postsRef.current = posts;
   sessionUserRef.current = sessionUser;
 
   const skipPromotedPosts = useMemo(() => {
@@ -92,16 +76,15 @@ const TabContent = ({
         return false;
     }
   }, [pageType]);
+
   // side effects
   useEffect(() => {
-
-    _initContent(true, feedUsername);
-
+    _initContent(feedUsername);
   }, [tag]);
 
   useEffect(() => {
     if (isConnected && (username !== sessionUser || forceLoadPosts)) {
-      _initContent(false, username);
+      _initContent(username);
     }
   }, [username, forceLoadPosts]);
 
@@ -119,128 +102,22 @@ const TabContent = ({
     if (pageType === 'ownProfile' && userPinned !== curPinned) {
       _scrollToTop();
       feedQuery.refresh();
-      // _loadPosts({ shouldReset: true, _pinnedPermlink: userPinned });
       setCurPinned(userPinned);
     }
   }, [userPinned]);
 
 
 
-  const _initContent = (isFirstCall = false, _feedUsername: string) => {
+  const _initContent = (_sessionUsername: string) => {
     _scrollToTop();
-
-    const initialPosts = isFirstCall && isFeedScreen && isInitialTab ? initPosts : [];
-
-    setPosts(initialPosts);
-    setTabMeta(DEFAULT_TAB_META);
-    setSessionUser(_feedUsername);
+    setSessionUser(_sessionUsername);
 
     if (postFetchTimerRef.current) {
       BackgroundTimer.clearTimeout(postFetchTimerRef.current);
       postFetchTimerRef.current = null;
     }
-
-    if (username || (filterKey !== 'friends' && filterKey !== 'communities')) {
-      // _loadPosts({
-      //   shouldReset: !isFirstCall,
-      //   isFirstCall,
-      //   isLatestPostsCheck: false,
-      //   _feedUsername,
-      //   _posts: initialPosts,
-      //   _tabMeta: DEFAULT_TAB_META,
-      // });
-      _getPromotedPosts();
-    }
   };
 
-  // fetch posts from server
-  const _loadPosts = async ({
-    shouldReset = false,
-    isLatestPostsCheck = false,
-    isFirstCall = false,
-    _feedUsername = isFeedScreen ? sessionUserRef.current : feedUsername,
-    _posts = postsRef.current,
-    _tabMeta = tabMeta,
-    _pinnedPermlink = curPinned,
-  }: {
-    shouldReset?: boolean;
-    isLatestPostsCheck?: boolean;
-    isFirstCall?: boolean;
-    _feedUsername?: string;
-    _posts?: any[];
-    _tabMeta?: TabMeta;
-    _pinnedPermlink?: string;
-  }) => {
-    const options = {
-      setTabMeta: (meta: TabMeta) => {
-        if (_isMounted) {
-          setTabMeta(meta);
-        }
-      },
-      filterKey,
-      prevPosts: _posts,
-      tabMeta: _tabMeta,
-      isLoggedIn,
-      nsfw,
-      isConnected,
-      isFeedScreen,
-      refreshing: shouldReset,
-      pageType,
-      isLatestPostsCheck,
-      feedUsername: _feedUsername,
-      pinnedPermlink: _pinnedPermlink,
-      tag,
-      ...props,
-    } as LoadPostsOptions;
-
-    const result = await loadPosts(options);
-    if (_isMounted && result) {
-      if (shouldReset || isFirstCall) {
-        setPosts([]);
-      }
-      // _postProcessLoadResult(result);
-    }
-  };
-
-  };
-
-
-
-  // processes response from loadPost
-  // const _postProcessLoadResult = ({ updatedPosts, latestPosts }: any) => {
-
-  //   // process returned data
-  //   if (Array.isArray(updatedPosts)) {
-  //     if (updatedPosts.length) {
-  //       // match new and old first post
-  //       const firstPostChanged =
-  //         posts.length == 0 || posts[0].permlink !== updatedPosts[0].permlink;
-  //       if (isFeedScreen && firstPostChanged) {
-  //         // schedule refetch of new posts by checking time of current post
-  //         _scheduleLatestPostsCheck(updatedPosts[0]);
-
-  //         if (isInitialTab) {
-  //           dispatch(setInitPosts(updatedPosts));
-  //         }
-  //       }
-  //     } else if (isFeedScreen && isInitialTab) {
-  //       // clear posts cache if no first tab posts available, precautionary measure for accoutn change
-  //       dispatch(setInitPosts([]));
-  //     }
-  //     setPosts(updatedPosts);
-  //   }
-  // };
-
-
-  // const _fetchLatestPosts = async () => {
-  //   const _latestPosts = await feedQuery.fetchLatestPosts();
-
-  //   if (_latestPosts.length > 0) {
-  //     setLatestPosts(_latestPosts);
-  //   } else {
-  //     // _scheduleLatestPostsCheck(posts[0]);
-  //   }
-  // }
 
   // view related routines
   const _onPostsPopupPress = () => {
@@ -266,7 +143,8 @@ const TabContent = ({
 
   // view rendereres
   const _renderEmptyContent = () => {
-    return <TabEmptyView filterKey={filterKey} isNoPost={tabMeta.isNoPost} />;
+    const _isNoPost = !feedQuery.isLoading && feedQuery.data.length == 0;
+    return <TabEmptyView filterKey={filterKey} isNoPost={_isNoPost} />;
   };
 
   const scrollPopupDebouce = debounce(
