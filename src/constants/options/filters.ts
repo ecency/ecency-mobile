3e29db9f051c755896335e358591a73c40cbb57b
noumantahir <nouman.hawkdev@gmail.com>
2024-09-20 19:55:25 +0500

    added user related tabs options, setting blog as default feed tab

--- src/constants/options/filters.ts
+++ src/constants/options/filters.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_FEED_FILTERS = ['friends', 'communities', 'hot'];
+export const DEFAULT_FEED_FILTERS = ['friends', 'communities', 'blog'];
 export const DEFAULT_COMMUNITY_FILTERS = ['trending', 'hot', 'created'];
 export const DEFAULT_PROFILE_FILTERS = ['blog', 'posts', 'comments'];
 export const DEFAULT_OWN_PROFILE_FILTERS = ['blog', 'posts', 'replies'];
@@ -9,6 +9,9 @@ export const FEED_SCREEN_FILTER_MAP = {
   trending: 'home.top',
   hot: 'home.hot',
   created: 'home.new',
+  blog: 'home.my_blog',
+  posts: 'home.my_posts',
+  comments: 'home.my_comments',
 };
 
 export const COMMUNITY_SCREEN_FILTER_MAP = {
@@ -30,9 +33,6 @@ export const PROFILE_SCREEN_FILTER_MAP = {
 export const GLOBAL_POST_FILTERS = ['home.TOP', 'home.HOT', 'home.NEW'];
 export const GLOBAL_POST_FILTERS_VALUE = ['trending', 'hot', 'created'];
 
-export const PROFILE_FILTERS_OWN = ['home.BLOG', 'home.POSTS', 'profile.replies'];
-export const PROFILE_FILTERS = ['home.BLOG', 'home.POSTS', 'profile.comments', 'profile.wallet'];
-export const PROFILE_FILTERS_VALUE = ['blog', 'posts', 'comments', 'wallet'];
 
 export const getFilterMap = (type: 'main' | 'community' | 'profile' | 'ownProfile') => {
   switch (type) {
