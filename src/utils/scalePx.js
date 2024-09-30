ca33b22f2d3ab2f0000e56cb8443beca2a40f12a
noumantahir <nouman.hawkdev@gmail.com>
2024-09-30 11:40:38 +0500

    avoid scaling size wrt widht ration

--- src/utils/scalePx.js
+++ src/utils/scalePx.js
@@ -1,16 +1,10 @@
 import { PixelRatio, Platform } from 'react-native';
-import getWindowDimensions from './getWindowDimensions';
 
-const { width: SCREEN_WIDTH } = getWindowDimensions();
-
-// based on iphone X's scale
-const scale = SCREEN_WIDTH / 414;
 
 export default (size) => {
-  const newSize = size * scale;
   if (Platform.OS === 'ios') {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+    return Math.round(PixelRatio.roundToNearestPixel(size));
   } else {
-    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+    return Math.round(PixelRatio.roundToNearestPixel(size)) - 2;
   }
 };
