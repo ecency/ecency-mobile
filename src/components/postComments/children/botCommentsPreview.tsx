a5151be4c700f9306e8104cdae757e218a3ce4ae
noumantahir <nouman.hawkdev@gmail.com>
2024-10-10 12:01:33 +0500

    design update

--- src/components/postComments/children/botCommentsPreview.tsx
+++ src/components/postComments/children/botCommentsPreview.tsx
@@ -1,41 +1,50 @@
 import React, { useRef } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
-import { CommentsModal, UserAvatar } from '../..';
+import { CommentsModal, Icon, UserAvatar } from '../..';
 import styles from '../styles/botCommentsPreview.styles';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
 interface BotCommentsProps {
-  comments: any[];
+    comments: any[];
 }
 
 export const BotCommentsPreview = ({ comments }: BotCommentsProps) => {
-  const commentsModalRef = useRef<typeof CommentsModal>();
+    const commentsModalRef = useRef<typeof CommentsModal>();
 
-  if(!comments?.length){
-    return null;
-  }
+    if (!comments?.length) {
+        return null;
+    }
 
-  return (
-    <View style={styles.container}>
-      {comments.map((comment) => {
-        return (
-          <TouchableOpacity
-            onPress={() => {
-              if (commentsModalRef.current) {
-                commentsModalRef.current.show(comments);
-              }
-            }}
-          >
-            <View style={styles.item}>
-              <UserAvatar username={comment.author} noAction />
-              <View style={styles.labelWrapper}>
-                <Text style={styles.label}>{comment.author}</Text>
-              </View>
+    return (
+        <View style={styles.container}>
+            <View style={styles.labelWrapper}>
+                <Text style={styles.label}>{"Bot Comments"}</Text>
+                <Icon
+                    iconType="AntDesign"
+                    name='right'
+                    size={14}
+                    color={EStyleSheet.value('$primaryDarkText')}
+                />
             </View>
-          </TouchableOpacity>
-        );
-      })}
+            <TouchableOpacity
+                onPress={() => {
+                    if (commentsModalRef.current) {
+                        commentsModalRef.current.show(comments);
+                    }
+                }}
+            >
+                <View style={styles.botAvatarsWrapper} >
+                    {comments.map((comment) => {
+                        return (
+                            <View style={styles.item}>
+                                <UserAvatar username={comment.author} noAction />
+                            </View>
+                        );
+                    })}
+                </View>
+            </TouchableOpacity>
 
-      <CommentsModal ref={commentsModalRef} />
-    </View>
-  );
+            <CommentsModal ref={commentsModalRef} />
+        </View>
+    );
 };
--- src/components/postComments/styles/botCommentsPreview.styles.ts
+++ src/components/postComments/styles/botCommentsPreview.styles.ts
@@ -2,28 +2,36 @@ import { ViewStyle } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
 export default EStyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    padding: 12,
-    paddingTop: 16,
-    alignItems: 'center',
-  } as ViewStyle,
-  item: {
-    padding: 4,
-    borderRadius: 24,
-    flexDirection: 'row',
-    alignItems: 'center',
-  } as ViewStyle,
-  labelWrapper: {
-    backgroundColor: '$primaryLightBackground',
-    padding: 4,
-    marginLeft: -8,
-    borderTopRightRadius: 24,
-    borderBottomRightRadius: 24,
-    zIndex: -1,
-  },
-  label: {
-    marginHorizontal: 8,
-    color: '$primaryDarkText',
-  },
+    container: {
+        flexDirection: 'row',
+        padding: 12,
+        paddingTop: 20,
+        alignItems: 'center',
+    } as ViewStyle,
+    botAvatarsWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 12,
+    } as ViewStyle,
+    item: {
+        marginLeft: -6,
+        borderRadius: 24,
+        flexDirection: 'row',
+        alignItems: 'center',
+    } as ViewStyle,
+    labelWrapper: {
+        backgroundColor: '$primaryLightBackground',
+        flexDirection:'row',
+        alignItems:'center',
+        padding: 4,
+        marginLeft: -12,
+        borderTopRightRadius: 24,
+        borderBottomRightRadius: 24,
+        paddingHorizontal:12
+    } as ViewStyle ,
+    label: {
+        paddingRight:8,
+        fontSize:16,
+        color: '$primaryDarkText',
+    },
 });
