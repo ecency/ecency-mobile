a5e0467f8deede8bc102ae002217300810dca45a
noumantahir <nouman.hawkdev@gmail.com>
2024-10-14 22:13:05 +0500

    merging cache with query data

--- src/providers/queries/proposalQueries.ts
+++ src/providers/queries/proposalQueries.ts
@@ -2,16 +2,18 @@ import { useQuery } from "@tanstack/react-query"
 import QUERIES from "./queryKeys"
 import { useSelector } from "react-redux"
 import { getProposalsVoted } from "../../providers/hive/dhive";
+import { useMemo } from "react";
 
 
-export const useProposalVotedQuery = (proposalId:number) => {
+export const useProposalVotedQuery = (proposalId: number) => {
 
     const currentAccount = useSelector(state => state.account.currentAccount);
+    const proposalsVoteMeta = useSelector(state => state.account.proposalsVoteMeta);
 
     const _getProposalVoteStatus = async () => {
         const votedProposals = await getProposalsVoted(currentAccount.username);
-        const isVoted =  votedProposals.some(item => item.proposal.proposal_id === proposalId)
-        
+        const isVoted = votedProposals.some(item => item.proposal.proposal_id === proposalId)
+
         console.log('is proposal voted', isVoted);
         return isVoted;
     }
@@ -19,10 +21,18 @@ export const useProposalVotedQuery = (proposalId:number) => {
     const query = useQuery(
         [QUERIES.PROPOSALS.GET_VOTES, currentAccount.name, proposalId],
         _getProposalVoteStatus,
-        {initialData:true}
+        { initialData: true }
     )
 
 
-    return query;
+    const data = useMemo(() => {
+        //form meta id
+        const cacheId = `${proposalId}_${currentAccount.username}`
+        return query.data || proposalsVoteMeta[cacheId].isVoted;
+    }, [query.data, proposalsVoteMeta])
 
+    return {
+        ...query,
+        data
+    };
 }
