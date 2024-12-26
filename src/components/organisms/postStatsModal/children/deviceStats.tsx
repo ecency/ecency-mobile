b9ea8d1100b670882f2e8fa52fcdded86c0d8659
noumantahir <nouman.hawkdev@gmail.com>
2024-12-26 18:22:46 +0500

    basic reveal animation for device stats

--- src/components/organisms/postStatsModal/children/deviceStats.tsx
+++ src/components/organisms/postStatsModal/children/deviceStats.tsx
@@ -4,12 +4,12 @@ import { usePostStatsByDevice } from '../../../../providers/queries';
 import styles from '../styles/deviceStats.styles';
 import { Bar as ProgressBar } from 'react-native-progress';
 import EStyleSheet from 'react-native-extended-stylesheet';
+import Animated, { SlideInLeft } from 'react-native-reanimated';
 
 export const DeviceStats = ({ urlPath }: { urlPath: string }) => {
   const statsByDevice = usePostStatsByDevice(urlPath);
 
   const dims = useWindowDimensions();
-
 
   const _barStyle = {
     borderRadius: 12,
@@ -18,17 +18,15 @@ export const DeviceStats = ({ urlPath }: { urlPath: string }) => {
     borderWidth:0
   } as ViewStyle
 
-
   const _barWidth = dims.width - 96;
-
 
   const total = useMemo(() =>
     statsByDevice.data?.reduce((prevVal, item) => prevVal + item.stats.pageviews, 0) || 1, [statsByDevice.data])
 
 return (
   <View style={styles.container}>
-    {statsByDevice.data?.map((stat) => (
-      <View key={stat.device} style={styles.statWrapper}>
+    {statsByDevice.data?.map((stat, index) => (
+      <Animated.View key={stat.device} style={styles.statWrapper} entering={SlideInLeft.delay(100 * (index + 1))}>
         <Text style={styles.statLabel}>{stat.device !== '(not set)' ? stat.device : 'Other'}</Text>
         <ProgressBar
           progress={stat.stats.pageviews / total}
@@ -40,7 +38,7 @@ return (
           indeterminate={statsByDevice.isLoading}
           useNativeDriver={true}
         />
-      </View>
+      </Animated.View>
     ))}
   </View>
 );
