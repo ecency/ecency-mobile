e11822f670140ece8e6359f730e782b4b70799aa
noumantahir <nouman.hawkdev@gmail.com>
2025-02-26 19:02:17 +0500

    migrated popover wrapper to use popover-view instead of modal-popover

--- src/components/popoverWrapper/popoverWrapperView.js
+++ src/components/popoverWrapper/popoverWrapperView.js
@@ -1,34 +1,34 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import { Popover, PopoverController } from 'react-native-modal-popover';
-
+import Popover from 'react-native-popover-view';
 import styles from './popoverWrapperStyles';
 
 const PopoverWrapper = ({ children, text }) => {
+
+  const _anchorComponent = (sourceRef, showPopover) => (
+    <TouchableOpacity
+      ref={sourceRef}
+      onPress={showPopover} >
+      {children}
+    </TouchableOpacity>
+  )
+
+  const _popoverContent = (
+    <View style={styles.popoverWrapper}>
+      <Text style={styles.popoverText}>{text}</Text>
+    </View>
+  )
+
   return (
-    <PopoverController>
-      {({ openPopover, closePopover, popoverVisible, setPopoverAnchor, popoverAnchorRect }) => (
-        <Fragment>
-          <TouchableOpacity ref={setPopoverAnchor} onPress={openPopover}>
-            {children}
-          </TouchableOpacity>
-          <Popover
-            backgroundStyle={styles.overlay}
-            contentStyle={styles.popoverDetails}
-            arrowStyle={styles.arrow}
-            visible={popoverVisible}
-            onClose={closePopover}
-            fromRect={popoverAnchorRect}
-            placement="top"
-            supportedOrientations={['portrait', 'landscape']}
-          >
-            <View style={styles.popoverWrapper}>
-              <Text style={styles.popoverText}>{text}</Text>
-            </View>
-          </Popover>
-        </Fragment>
-      )}
-    </PopoverController>
+    <Popover
+      from={_anchorComponent} // Anchor reference
+      backgroundStyle={styles.overlay} // Overlay style
+      popoverStyle={styles.popoverDetails} // Popover content style
+      arrowStyle={styles.arrow} // Arrow style
+    >
+      {_popoverContent}
+    </Popover>
+
   );
 };
 
