2ca53590c93ebf824ca6ac9e4583e13639dc9375
noumantahir <nouman.hawkdev@gmail.com>
2025-05-15 12:51:21 +0500

    using translated text

--- src/components/postCard/children/crossPostLabel.tsx
+++ src/components/postCard/children/crossPostLabel.tsx
@@ -1,95 +1,98 @@
 import React from 'react';
 import { View, Text, TextStyle } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
-import { PostCardActionIds } from '../container/postCard';
-import Icon from '../../../components/icon';
-import ROUTES from '../../../constants/routeNames';
-import styles from '../styles/children.styles'
 import { useState, useEffect } from 'react';
+import { useIntl } from 'react-intl';
+import { PostCardActionIds } from '../container/postCard';
+import Icon from '../../icon';
+import ROUTES from '../../../constants/routeNames';
+import styles from '../styles/children.styles';
 import { getCommunityTitle } from '../../../providers/hive/dhive';
 
-
 interface CrossPostLabelProps {
-    crosspostMeta: {
-        author: string;
-        community: string;
-    };
-    handleCardInteraction: (id: PostCardActionIds, payload?: any) => void;
+  crosspostMeta: {
+    author: string;
+    community: string;
+  };
+  handleCardInteraction: (id: PostCardActionIds, payload?: any) => void;
 }
 
-const CrossPostLabel: React.FC<CrossPostLabelProps> = ({ crosspostMeta, handleCardInteraction }) => {
-    if (!crosspostMeta) {
-        return null;
-    }
+const CrossPostLabel: React.FC<CrossPostLabelProps> = ({
+  crosspostMeta,
+  handleCardInteraction,
+}) => {
+  if (!crosspostMeta) {
+    return null;
+  }
 
-    const { author, community } = crosspostMeta;
+  const intl = useIntl();
 
-    const [communityTitle, setCommunityTitle] = useState<string | null>(null);
+  const { author, community } = crosspostMeta;
 
-    useEffect(() => {
-        const fetchCommunityTitle = async () => {
-            try {
-                if (!communityTitle) {
-                    const title = await getCommunityTitle(community);
-                    setCommunityTitle(title);
-                }
-            } catch (error) {
-                console.error('Failed to fetch community title:', error);
-            }
-        };
+  const [communityTitle, setCommunityTitle] = useState<string | null>(null);
 
-        fetchCommunityTitle();
-    }, [community]);
+  useEffect(() => {
+    const fetchCommunityTitle = async () => {
+      try {
+        if (!communityTitle) {
+          const title = await getCommunityTitle(community);
+          setCommunityTitle(title);
+        }
+      } catch (error) {
+        console.error('Failed to fetch community title:', error);
+      }
+    };
 
+    fetchCommunityTitle();
+  }, [community]);
 
+  const _handleOnTagPress = () => {
+    handleCardInteraction(PostCardActionIds.NAVIGATE, {
+      name: ROUTES.SCREENS.COMMUNITY,
+      params: {
+        tag: crosspostMeta.community,
+      },
+    });
+  };
 
-    const _handleOnTagPress = () => {
-        handleCardInteraction(PostCardActionIds.NAVIGATE, {
-            name: ROUTES.SCREENS.COMMUNITY,
-            params: {
-                tag: crosspostMeta.community,
-            },
-        });
-    }
+  const _handleOnUserPress = () => {
+    handleCardInteraction(PostCardActionIds.USER, author);
+  };
 
+  const _highlightFontStyle: TextStyle = {
+    fontWeight: '700',
+    color: EStyleSheet.value('$primaryBlue'),
+  };
 
-    const _handleOnUserPress = () => {
-        handleCardInteraction(PostCardActionIds.USER, author)
-    }
+  return (
+    <View style={styles.crossPostWrapper}>
+      <Icon
+        iconType="MaterialIcons"
+        name="repeat"
+        size={16}
+        color={EStyleSheet.value('$iconColor')}
+        style={{ marginRight: 4 }}
+      />
 
-
-    const _highlightFontStyle: TextStyle = { fontWeight: '700', color: EStyleSheet.value('$primaryBlue') }
-
-    return (
-        <View style={styles.crossPostWrapper}>
-            <Icon
-                iconType="MaterialIcons"
-                name="repeat"
-                size={16}
-                color={EStyleSheet.value('$iconColor')}
-                style={{ marginRight: 4 }}
-            />
-            <Text
-                style={styles.repostText}
-                onPress={_handleOnUserPress}
-            >
-                {"cross post by "}
-                <Text
-                    style={_highlightFontStyle}
-                    onPress={_handleOnTagPress}>
-                    {author}
-                </Text>
-
-                {" in "}
-                <Text
-                    style={_highlightFontStyle}
-                    onPress={_handleOnTagPress}>
-                    {communityTitle || community}
-                </Text>
-            </Text>
-        </View>
-    );
+      <Text style={styles.repostText}>
+        {intl.formatMessage(
+          { id: 'post.cross_post' },
+          {
+            username: (
+              <Text style={_highlightFontStyle} onPress={_handleOnUserPress}>
+                {author}
+              </Text>
+            ),
+            community: (
+              <Text style={_highlightFontStyle} onPress={_handleOnTagPress}>
+                {communityTitle || community}
+              </Text>
+            ),
+          },
+        )}
+      </Text>
+    </View>
+  );
 };
-
 
 export default CrossPostLabel;
