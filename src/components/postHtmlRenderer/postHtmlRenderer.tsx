c62931d3331a8e4ef53ef2755a0362d3243fbf55
feruz <fmiboy@gmail.com>
2025-08-06 09:45:08 +0300

    preserve full style, touchable

--- src/components/postHtmlRenderer/postHtmlRenderer.tsx
+++ src/components/postHtmlRenderer/postHtmlRenderer.tsx
@@ -3,7 +3,7 @@ import RenderHTML, { CustomRendererProps, Element, TNode } from 'react-native-re
 import { useHtmlIframeProps, iframeModel } from '@native-html/iframe-plugin';
 import WebView from 'react-native-webview';
 import { ScrollView } from 'react-native-gesture-handler';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import styles from './postHtmlRendererStyles';
 import { LinkData, parseLinkData } from './linkDataParser';
 import VideoThumb from './videoThumb';
@@ -331,18 +331,18 @@ export const PostHtmlRenderer = memo(
       const paragraphText = extractTextFromTNode(tnode).trim();
 
       const handleLongPress = () => {
-        setSelectedText(paragraphText);
-        setModalVisible(true);
+        if (paragraphText) {
+          setSelectedText(paragraphText);
+          setModalVisible(true);
+        }
       };
 
       return (
-        <Text
-          selectable={false}
-          style={isInsideLi ? styles.pLi : styles.p}
-          onLongPress={handleLongPress}
-        >
-          <props.InternalRenderer {...props} />
-        </Text>
+        <TouchableWithoutFeedback onLongPress={handleLongPress}>
+          <Text selectable={false} style={isInsideLi ? styles.pLi : styles.p}>
+            <props.InternalRenderer {...props} />
+          </Text>
+        </TouchableWithoutFeedback>
       );
     };
 
