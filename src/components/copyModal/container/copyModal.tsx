ba1f6bab085fff517a55e691cc5eed73f574200b
noumantahir <nouman.hawkdev@gmail.com>
2025-08-13 13:16:54 +0500

    updated copy modal to have extended ref

--- src/components/copyModal/container/copyModal.tsx
+++ src/components/copyModal/container/copyModal.tsx
@@ -1,41 +1,49 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useImperativeHandle, forwardRef, useState, useRef } from 'react';
 import { TextInput } from 'react-native';
 import Popover from 'react-native-popover-view';
 import { Placement } from 'react-native-popover-view/dist/Types';
 import { useIntl } from 'react-intl';
 import { MainButton } from '../../mainButton';
-
 import styles from '../styles/copyModal.styles';
 
-export const CopyModal = ({
-  visible,
-  onClose,
-  text,
-  sourceRef,
-}: {
-  visible: boolean;
-  onClose: () => void;
-  text: string;
-}) => {
-  const inputRef = useRef<TextInput>(null);
-  const intl = useIntl();
 
-  useEffect(() => {
-    if (visible) {
-      setTimeout(() => {
-        inputRef.current?.focus();
-      }, 0);
+export type CopyModalHandle = {
+    show: (selectedText:string) => void;
+};
+
+const CopyModalInner =  (
+    {},
+    ref: React.Ref<CopyModalHandle>
+) => {
+    const intl = useIntl();
+    const inputRef = useRef<TextInput>(null);
+
+    const [isVisible, setIsVisible] = useState(false);
+    const [text, setText] = useState('');
+
+    useImperativeHandle(ref, () => ({
+        show: (selectedText) => {
+            setIsVisible(true);
+            setText(selectedText)
+             setTimeout(() => {
+                    inputRef.current?.focus();
+            }, 0);
+        }
+    }));
+
+
+    const _onClose = () => {
+        setIsVisible(false);
+        setText('')
     }
-  }, [visible, text]);
-
-  return (
+ 
+    return (
     <Popover
       popoverStyle={styles.modal}
       arrowSize={{ width: 0, height: 0 }}
-      isVisible={visible}
-      onRequestClose={onClose}
+      isVisible={isVisible}
+      onRequestClose={_onClose}
       backgroundStyle={{ flex: 1 }}
-      from={sourceRef}
       placement={[Placement.CENTER]}
       offset={12}
     >
@@ -52,9 +60,12 @@ export const CopyModal = ({
       <MainButton
         style={styles.button}
         textStyle={styles.buttonText}
-        onPress={onClose}
+        onPress={_onClose}
         text={intl.formatMessage({ id: 'alert.close' })}
       />
     </Popover>
   );
 };
+
+
+export const CopyModal = forwardRef(CopyModalInner);
