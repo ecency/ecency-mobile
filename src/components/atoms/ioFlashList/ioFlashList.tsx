f1e738cf77332c3bb45a245a2fcd0d31a8ca051c
noumantahir <nouman.hawkdev@gmail.com>
2025-08-16 15:29:47 +0500

    added flast list wrapped in IO

---
+++ src/components/atoms/ioFlashList/ioFlashList.tsx
@@ -0,0 +1,20 @@
+import { ForwardRefExoticComponent, RefAttributes } from 'react';
+import { type IOComponentProps, withIO } from 'react-native-intersection-observer';
+import { FlashList, FlashListProps } from '@shopify/flash-list';
+
+export type IOFlashListController = FlashList<any>;
+export type IOFlashListProps<ItemT> = IOComponentProps & FlashListProps<ItemT>;
+
+const IOFlashList = withIO(FlashList, [
+    'scrollToIndex',
+    'scrollToOffset',
+    'scrollToEnd',
+    'getScrollResponder',
+    'getScrollableNode',
+    'recordInteraction',
+    'flashScrollIndicators',
+]);
+
+export default IOFlashList as unknown as ForwardRefExoticComponent<
+    IOFlashListProps<any> & RefAttributes<IOFlashListController>
+>;
