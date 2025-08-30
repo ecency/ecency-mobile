92704909eac04c2cdacb65addb81c17e4f6eb2d5
noumantahir <nouman.hawkdev@gmail.com>
2025-08-30 20:00:47 +0500

    simplified repies reveal animation

--- src/components/postComments/children/commentsSection.tsx
+++ src/components/postComments/children/commentsSection.tsx
@@ -1,48 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
-
-// Components
-// import Animated, { SlideInRight } from 'react-native-reanimated';
 import { Comment } from '../..';
+import Animated, { LinearTransition, Easing } from 'react-native-reanimated';
 
 export const CommentsSection = ({ item, ...props }) => {
-  const toggleRef = React.useRef(false);
-  const [toggle, setToggle] = useState(item.expandedReplies || toggleRef.current);
+  const [toggle, setToggle] = useState(item.expandedReplies || false);
 
   useEffect(() => {
     if (item.expandedReplies) {
       setToggle(true);
-      toggleRef.current = true;
     }
   }, [item.expandedReplies]);
 
   const _renderComment = (item, index = 0) => {
-    // animation makes sure there is 100 ms gab between each comment item
-    // const _enteringAnim =
-    //   index >= 0
-    //     ? SlideInRight.duration(150)
-    //         .springify()
-    //         .delay(index * 100)
-    //     : undefined;
-
     return (
-      // <Animated.View key={item.author + item.permlink} entering={_enteringAnim}>
       <View key={item.author + item.permlink}>
         <Comment
           comment={item}
           repliesToggle={toggle}
-          handleOnToggleReplies={() => {toggleRef.current = !toggle; setToggle(!toggle)}}
+          handleOnToggleReplies={() => { setToggle(!toggle) }}
           {...props}
         />
-        </View>
-      // </Animated.View>
+      </View>
     );
   };
+
+
+  const _renderReplies = () => {
+
+    return (
+      <Animated.View layout={LinearTransition.easing(Easing.ease).duration(200)} style={{ overflow: 'hidden' }}  >
+        {toggle && item.repliesThread.map((reply, index) => _renderComment(reply, index))}
+      </Animated.View>
+    )
+  }
 
   return (
     <>
       {_renderComment(item, -1)}
-      {toggle && item.repliesThread.map((reply, index) => _renderComment(reply, index))}
+      {_renderReplies()}
     </>
   );
 };
