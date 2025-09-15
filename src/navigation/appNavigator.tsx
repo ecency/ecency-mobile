797b85b292d22de79c46379ad7426c65a5d54f62
noumantahir <nouman.hawkdev@gmail.com>
2025-09-15 13:46:58 +0500

    processing initial deep link on Navigation ready event

--- src/navigation/appNavigator.tsx
+++ src/navigation/appNavigator.tsx
@@ -2,21 +2,30 @@ import React, { useState } from 'react';
 import VersionNumber from 'react-native-version-number';
 import { NavigationContainer } from '@react-navigation/native';
 import RNBootSplash from 'react-native-bootsplash';
-import { useAppSelector } from '../hooks';
+import { useAppSelector, useLinkProcessor } from '../hooks';
 
 // Screens
 import { StackNavigator } from './stackNavigator';
 import { navigationRef } from './rootNavigation';
 import ROUTES from '../constants/routeNames';
 import parseVersionNumber from '../utils/parseVersionNumber';
+import { Linking } from 'react-native';
 
 export const AppNavigator = () => {
   const lastAppVersion = useAppSelector((state) => state.application.lastAppVersion);
+  const linkProcessor = useLinkProcessor();
 
   const [appVersion] = useState(VersionNumber.appVersion);
 
   const _onReady = () => {
     RNBootSplash.hide({ fade: true });
+
+    //read initial URL
+    Linking.getInitialURL().then((url) => {
+      if (url) {
+        linkProcessor.handleLink(url);
+      }
+    });
   };
 
   const _initRoute =
