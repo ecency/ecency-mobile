cee482af54a6d970b7156462e7dc715692a29ede
feruz <fmiboy@gmail.com>
2026-01-26 18:51:36 +0200

    performance: link callbacks

--- src/components/searchModal/container/searchModalContainer.tsx
+++ src/components/searchModal/container/searchModalContainer.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import { connect } from 'react-redux';
 import get from 'lodash/get';
+import debounce from 'lodash/debounce';
 
 // Services and Actions
 import { useNavigation } from '@react-navigation/native';
@@ -41,137 +42,159 @@ const SearchModalContainer = ({ isConnected, handleOnClose, isOpen, placeholder 
     navigation.goBack();
   };
 
-  const _handleOnChangeSearchInput = (text) => {
-    if (text && text.length < 3) {
-      return;
-    }
-    if (!isConnected) {
-      return;
-    }
-    if (text && text !== '@' && text !== '#') {
-      if (text[0] === '@') {
-        queryClient
-          .fetchQuery(lookupAccountsQueryOptions(text.substr(1).trim()))
-          .then((res) => {
-            const users = res
-              ? res.map((item) => ({
-                  image: getResizedAvatar(item),
-                  text: item,
-                  ...item,
-                }))
-              : [];
-            setSearchResults({ type: 'user', data: users });
-          })
-          .catch((e) => console.log('lookupAccounts', e));
-      } else if (text[0] === '#') {
-        queryClient
-          .fetchQuery(getTrendingTagsQueryOptions(text.substr(1).trim(), 20))
-          .then((res) => {
-            const tags = res
-              ? res.map((item) => ({
-                  text: `#${get(item, 'name', '')}`,
-                  ...item,
-                }))
-              : [];
-
-            setSearchResults({ type: 'tag', data: tags });
-          })
-          .catch((e) => console.log('getTrendingTags', e));
-      } else if (
-        text.includes('https://') ||
-        text.includes('esteem://') ||
-        text.includes('ecency://')
-      ) {
-        const postUrl = postUrlParser(text.replace(/\s/g, ''));
-
-        if (postUrl) {
-          const { author, permlink, feedType, tag } = postUrl;
-
-          if (author) {
-            if (permlink) {
-              queryClient
-                .fetchQuery(getPostQueryOptions(author, permlink, ''))
-                .then((post) => {
-                  if (post.post_id !== 0) {
-                    const result = {};
-                    let metadata = {};
-                    try {
-                      metadata = JSON.parse(get(post, 'json_metadata', ''));
-                    } catch (error) {
-                      metadata = {};
-                    }
-                    if (get(metadata, 'image', false) && metadata.image.length > 0) {
-                      [result.image] = metadata.image;
-                    } else {
-                      result.image = getResizedAvatar(author);
-                    }
-                    result.author = author;
-                    result.text = post.title;
-                    result.permlink = permlink;
-                    setSearchResults({ type: 'content', data: [result] });
-                  } else {
-                    setSearchResults({ type: 'content', data: [] });
-                  }
-                })
-                .catch((e) => console.log('getPurePost', e));
-            } else {
-              queryClient
-                .fetchQuery(lookupAccountsQueryOptions(author))
-                .then((res) => {
-                  const users = res.map((item) => ({
+  // Core search logic (will be debounced)
+  const performSearch = useCallback(
+    (text) => {
+      if (text && text.length < 3) {
+        return;
+      }
+      if (!isConnected) {
+        return;
+      }
+      if (text && text !== '@' && text !== '#') {
+        if (text[0] === '@') {
+          queryClient
+            .fetchQuery(lookupAccountsQueryOptions(text.substr(1).trim()))
+            .then((res) => {
+              const users = res
+                ? res.map((item) => ({
                     image: getResizedAvatar(item),
                     text: item,
                     ...item,
-                  }));
-                  setSearchResults({ type: 'user', data: users });
-                })
-                .catch((e) => console.log('lookupAccounts', e));
-            }
-          } else if (feedType) {
-            // handleOnClose();
-            // setSearchResults({});
-            if (tag) {
-              setSearchResults({
-                type: 'feedType',
-                data: [{ text: `#${tag}`, tag, filter: feedType }],
-              });
-              // navigation.navigate({
-              //   name: ROUTES.SCREENS.SEARCH_RESULT,
-              //   params: {
-              //     tag: tag,
-              //     filter: feedType,
-              //   },
-              // });
-            } else {
-              setSearchResults({
-                type: 'feedType',
-                data: [{ text: `#${feedType}`, filter: feedType }],
-              });
-              // navigation.navigate({
-              //   name: ROUTES.SCREENS.SEARCH_RESULT,
-              //   params: {
-              //     filter: feedType,
-              //   },
-              // });
+                  }))
+                : [];
+              setSearchResults({ type: 'user', data: users });
+            })
+            .catch((e) => console.log('lookupAccounts', e));
+        } else if (text[0] === '#') {
+          queryClient
+            .fetchQuery(getTrendingTagsQueryOptions(text.substr(1).trim(), 20))
+            .then((res) => {
+              const tags = res
+                ? res.map((item) => ({
+                    text: `#${get(item, 'name', '')}`,
+                    ...item,
+                  }))
+                : [];
+
+              setSearchResults({ type: 'tag', data: tags });
+            })
+            .catch((e) => console.log('getTrendingTags', e));
+        } else if (
+          text.includes('https://') ||
+          text.includes('esteem://') ||
+          text.includes('ecency://')
+        ) {
+          const postUrl = postUrlParser(text.replace(/\s/g, ''));
+
+          if (postUrl) {
+            const { author, permlink, feedType, tag } = postUrl;
+
+            if (author) {
+              if (permlink) {
+                queryClient
+                  .fetchQuery(getPostQueryOptions(author, permlink, ''))
+                  .then((post) => {
+                    if (post.post_id !== 0) {
+                      const result = {};
+                      let metadata = {};
+                      try {
+                        metadata = JSON.parse(get(post, 'json_metadata', ''));
+                      } catch (error) {
+                        metadata = {};
+                      }
+                      if (get(metadata, 'image', false) && metadata.image.length > 0) {
+                        [result.image] = metadata.image;
+                      } else {
+                        result.image = getResizedAvatar(author);
+                      }
+                      result.author = author;
+                      result.text = post.title;
+                      result.permlink = permlink;
+                      setSearchResults({ type: 'content', data: [result] });
+                    } else {
+                      setSearchResults({ type: 'content', data: [] });
+                    }
+                  })
+                  .catch((e) => console.log('getPurePost', e));
+              } else {
+                queryClient
+                  .fetchQuery(lookupAccountsQueryOptions(author))
+                  .then((res) => {
+                    const users = res.map((item) => ({
+                      image: getResizedAvatar(item),
+                      text: item,
+                      ...item,
+                    }));
+                    setSearchResults({ type: 'user', data: users });
+                  })
+                  .catch((e) => console.log('lookupAccounts', e));
+              }
+            } else if (feedType) {
+              // handleOnClose();
+              // setSearchResults({});
+              if (tag) {
+                setSearchResults({
+                  type: 'feedType',
+                  data: [{ text: `#${tag}`, tag, filter: feedType }],
+                });
+                // navigation.navigate({
+                //   name: ROUTES.SCREENS.SEARCH_RESULT,
+                //   params: {
+                //     tag: tag,
+                //     filter: feedType,
+                //   },
+                // });
+              } else {
+                setSearchResults({
+                  type: 'feedType',
+                  data: [{ text: `#${feedType}`, filter: feedType }],
+                });
+                // navigation.navigate({
+                //   name: ROUTES.SCREENS.SEARCH_RESULT,
+                //   params: {
+                //     filter: feedType,
+                //   },
+                // });
+              }
             }
           }
+        } else {
+          search({ q: text })
+            .then((res) => {
+              res.results = res.results
+                .filter((item) => item.title !== '')
+                .map((item) => ({
+                  image: item.img_url || getResizedAvatar(get(item, 'author')),
+                  text: item.title,
+                  ...item,
+                }));
+              setSearchResults({ type: 'content', data: get(res, 'results', []) });
+            })
+            .catch((e) => console.log('search', e));
         }
-      } else {
-        search({ q: text })
-          .then((res) => {
-            res.results = res.results
-              .filter((item) => item.title !== '')
-              .map((item) => ({
-                image: item.img_url || getResizedAvatar(get(item, 'author')),
-                text: item.title,
-                ...item,
-              }));
-            setSearchResults({ type: 'content', data: get(res, 'results', []) });
-          })
-          .catch((e) => console.log('search', e));
       }
-    }
-  };
+    },
+    [isConnected, queryClient],
+  );
+
+  // Create debounced version of search with 500ms delay
+  const debouncedSearch = useMemo(() => debounce(performSearch, 500), [performSearch]);
+
+  // Cleanup debounce on unmount
+  useEffect(() => {
+    return () => {
+      debouncedSearch.cancel();
+    };
+  }, [debouncedSearch]);
+
+  // Handler called on input change - triggers debounced search
+  const _handleOnChangeSearchInput = useCallback(
+    (text) => {
+      debouncedSearch(text);
+    },
+    [debouncedSearch],
+  );
 
   const _handleOnPressListItem = (type, item) => {
     let name = null;
